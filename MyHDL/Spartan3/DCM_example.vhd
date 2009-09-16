cde2d73ac397dfb05c8e1173714e2b272a55ab6e
stan <stan@crds-d620-1>
2009-09-15 18:36:06 -0700

    Include files for building FPGA and DSP code

--- MyHDL/Spartan3/DCM_example.vhd
+++ MyHDL/Spartan3/DCM_example.vhd
@@ -7,7 +7,7 @@
 -- \   \   \/     Version : 10.1.03
 --  \   \         Application : xaw2vhdl
 --  /   /         Filename : DCM_example.vhd
--- /___/   /\     Timestamp : 09/03/2009 18:36:30
+-- /___/   /\     Timestamp : 09/15/2009 18:01:05
 -- \   \  /  \ 
 --  \___\/\___\ 
 --
