9f4a333549b792c099b91a3b97e23077bdc4d6d7
stan <stan@crds-d620-1>
2009-10-02 18:32:45 -0700

    Added script for re-indenting DSP source files

---
+++ DSP/reindent.bat
@@ -0,0 +1,8 @@
+call call_Artistic_Style autogen\*.c
+call call_Artistic_Style autogen\*.h
+call call_Artistic_Style common\*.c
+call call_Artistic_Style common\*.h
+call call_Artistic_Style common\*.c
+call call_Artistic_Style common\*.h
+call call_Artistic_Style registerTest\*.c
+call call_Artistic_Style registerTest\*.h
