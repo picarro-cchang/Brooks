1705acb93b239b1a34a6af4b82db05ca36999b14
stan <stan@crds-d620-1>
2010-07-19 15:24:10 -0700

    Corrected error in writing EEPROMs on board tester

--- DSP/common/i2cEeprom.c
+++ DSP/common/i2cEeprom.c
@@ -21,35 +21,55 @@
 #include "i2cEeprom.h"
 #include "registers.h"
 
-static int diag_bit = 0;
+static int eeprom_send_byte_address(I2C_device *i2c,unsigned char address)
+{
+    int loops;
+    unsigned char data[1];
+    data[0] = address;
+    for (loops=0; loops<10000; loops++);
+    return I2C_write_bytes(hI2C[i2c->chain],i2c->addr,data,1);
+}
 
-int eeprom_send_address(I2C_device *i2c,unsigned short address)
+static int eeprom_send_address(I2C_device *i2c,unsigned short address)
 {
     int loops;
     unsigned char data[2];
     data[0] = (address>>8) & 0xFF;
     data[1] = address & 0xFF;
     for (loops=0; loops<10000; loops++);
-    // Toggle bit 3 of kernel diag register for debugging I2C EEPROM
-    diag_bit = !diag_bit;
-    changeBitsFPGA(FPGA_KERNEL+KERNEL_DIAG_1, 3, 1, diag_bit);
     return I2C_write_bytes(hI2C[i2c->chain],i2c->addr,data,2);
 }
 
 int eeprom_write(I2C_device *i2c,unsigned short address,unsigned char *buffer, int nbytes)
-// Write up to 64 bytes to the EEPROM at location address. These data must all fit in a single page.
+// If nbytes>0, write up to 64 bytes to the EEPROM at location address. A two byte address is used.
+// If nbytes<0, write -nbytes bytes (<=16) to the EEPROM at the location address. A single byte address is used.
+//  These data must all fit in a single page.
 //  Call eeprom_busy to check when data have been written.
 {
-    unsigned short lastAddress = address+nbytes-1;
+    unsigned short lastAddress;
     int stat;
     
-    if ((address & 0xFFC0) != (lastAddress & 0xFFC0)) {
-        message_puts("Page error in eeprom_write");
-        return I2C_BADPAGE;
-    }
-    if (0 != (stat = eeprom_send_address(i2c,address))) {
-        message_puts("eeprom_send_address failed in eeprom_write");
-        return stat;
+    if (nbytes>0) {
+        lastAddress = address+nbytes-1;
+        if ((address & 0xFFC0) != (lastAddress & 0xFFC0)) {
+            message_puts("Page error in eeprom_write");
+            return I2C_BADPAGE;
+        }
+        if (0 != (stat = eeprom_send_address(i2c,address))) {
+            message_puts("eeprom_send_address failed in eeprom_write");
+            return stat;
+        }
+    } else {
+        nbytes = -nbytes;
+        lastAddress = address+nbytes-1;
+        if ((address & 0xFFF0) != (lastAddress & 0xFFF0)) {
+            message_puts("Page error in eeprom_write");
+            return I2C_BADPAGE;
+        }
+        if (0 != (stat = eeprom_send_byte_address(i2c,(unsigned char)address))) {
+            message_puts("eeprom_send_byte_address failed in eeprom_write");
+            return stat;
+        }
     }
     // We need to write the data immediately following the address, with no I2C address info
     stat = I2C_write_bytes_nostart(hI2C[i2c->chain],buffer,nbytes);
@@ -69,10 +89,16 @@ int eeprom_busy(I2C_device *i2c)
 
 int eeprom_read(I2C_device *i2c,unsigned short address,unsigned char *buffer, int nbytes)
 {
+// If nbytes>0, read bytes from the EEPROM at location address. A two byte address is used.
+// If nbytes<0, read -nbytes bytes from the EEPROM at the location address. A single byte address is used.
     if (eeprom_busy(i2c)) {
         message_puts("Device busy in eeprom_read");
         return I2C_BUSY;
     }
-    eeprom_send_address(i2c,address);
+    if (nbytes>0) eeprom_send_address(i2c,address);
+    else {
+        eeprom_send_byte_address(i2c,(unsigned char)address);
+        nbytes = -nbytes;
+    }
     return I2C_read_bytes(hI2C[i2c->chain],i2c->addr,buffer,nbytes);
 }
