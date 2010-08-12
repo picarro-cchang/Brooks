316644ca1187597f8f2c8ea14c4de31672c0eefe
picarro <picarro@MI910-G2000a>
2010-08-12 15:26:08 -0700

    Changed the version number to 0.0.3

--- Common/version.py
+++ Common/version.py
@@ -16,7 +16,7 @@
 #                                                            
 HOST_MAJOR_VERSION = 0
 HOST_MINOR_VERSION = 0
-HOST_INTERNAL_VERSION = "2"
+HOST_INTERNAL_VERSION = "3"
 
 def versionString():
     if HOST_INTERNAL_VERSION in [0, "0"]:
