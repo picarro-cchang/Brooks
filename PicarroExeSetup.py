8186fc2ee41cbebeddb5db6c9915331025a950bb
stan <stan@crds-d620-1>
2010-08-17 14:31:21 -0700

    Include tables.* and encodings.* in PicarroExeSetup.py

--- PicarroExeSetup.py
+++ PicarroExeSetup.py
@@ -148,7 +148,8 @@ inclusionList = ["email","email.iterators","email.generator","email.mime.audio",
                  "email.mime.base","scipy.interpolate","scipy.misc",
                  "sip", "matplotlib.backends",  "matplotlib.backends.backend_wxagg",
                  "matplotlib.figure","pylab", "numpy", "matplotlib.numerix.fft",
-                 "matplotlib.numerix.linear_algebra", "matplotlib.numerix.random_array", "configobj"]
+                 "matplotlib.numerix.linear_algebra", "matplotlib.numerix.random_array", "configobj",
+                 "encodings.*", "tables.*" ]
 dllexclusionList = ['libgdk-win32-2.0-0.dll', 'libgobject-2.0-0.dll']
 
 hex_images = glob.glob("../SrcCode/CypressUSB/Drivers/*.*")
