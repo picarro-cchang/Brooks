ba0350e98da8788c02ebec92a492be12c733328d
picarro <picarro@MI910-G2000a>
2010-08-26 12:38:08 -0700

    Added clock symbol when application failed to launched withint timeout (30 seconds)

--- PicarroExeSetup.py
+++ PicarroExeSetup.py
@@ -237,7 +237,8 @@ setup(version = "1.0",
                            "QuickGui/LEDred2.ico",
                            "QuickGui/logo.png",
                            "Fitter/fitutils.pyd",
-                           "Utilities/SupervisorLauncher/Check.png"]),
+                           "Utilities/SupervisorLauncher/Check.png",
+                           "Utilities/SupervisorLauncher/alarm.png"]),
                     (r'mpl-data', glob.glob(r'C:\Python25\Lib\site-packages\matplotlib\mpl-data\*.*')),
                     # Because matplotlibrc does not have an extension, glob does not find it (at least I think that's why)
                     # So add it manually here:
--- Utilities/SupervisorLauncher/HostStartup.py
+++ Utilities/SupervisorLauncher/HostStartup.py
@@ -8,22 +8,24 @@ import time
 import getopt
 from configobj import ConfigObj
 
-taskList = [
-"Supervisor.exe",
-"EventManager.exe",
-"Driver.exe",
-"Archiver.exe",
-"RDFrequencyConverter.exe",
-"SpectrumCollector.exe",
-"Fitter.exe",
-"MeasSystem.exe",
-"DataManager.exe",
-"SampleManager.exe",
-"InstMgr.exe",
-"AlarmSystem.exe",
-"ValveSequencer.exe",
-"QuickGui.exe",
-]
+translationTable = {
+"supervisor.exe": "Supervisor",
+"eventmanager.exe": "Event Manager",
+"driver.exe": "Driver",
+"archiver.exe": "Data Archiver",
+"rdfrequencyconverter.exe": "Ring-down Frequency Converter",
+"spectrumcollector.exe": "Spectrum Collector",
+"fitter.exe": "Fitter",
+"meassystem.exe": "Measurement System",
+"datamanager.exe": "Data Manager",
+"samplemanager.exe": "Sample Manager",
+"instmgr.exe": "Instrument Manager",
+"alarmsystem.exe": "Alarm System",
+"valvesequencer.exe": "Valve Sequencer",
+"quickgui.exe": "CRDS Data Viewer"
+}
+
+appTimeout = 30 # sec
 
 #Set up a useful AppPath reference...
 if hasattr(sys, "frozen"): #we're running compiled with py2exe
@@ -49,27 +51,31 @@ class HostStartupFrame(wx.Frame):
     def __init__(self, taskList, *args, **kwds):
         self.taskList = taskList
         kwds["style"] = wx.CAPTION|wx.CLOSE_BOX|wx.MINIMIZE_BOX|wx.SYSTEM_MENU|wx.TAB_TRAVERSAL|wx.STAY_ON_TOP
+        kwds["pos"] = (0,50)
         wx.Frame.__init__(self, *args, **kwds)
         self.panel1 = wx.Panel(self, -1, style=wx.TAB_TRAVERSAL|wx.ALWAYS_SHOW_SB)
         self.panel2 = wx.Panel(self, -1, style=wx.TAB_TRAVERSAL|wx.ALWAYS_SHOW_SB)
         self.panel3 = wx.Panel(self, -1, style=wx.TAB_TRAVERSAL|wx.ALWAYS_SHOW_SB)
-        self.panel4 = wx.Panel(self, -1, style=wx.TAB_TRAVERSAL|wx.ALWAYS_SHOW_SB)
         self.panel1.SetBackgroundColour("white")
         self.panel2.SetBackgroundColour("#E0FFFF")
         self.panel3.SetBackgroundColour("#BDEDFF")
-        self.panel4.SetBackgroundColour("white")
         self.SetTitle("Picarro Software Loading Status")
+        self.CenterOnScreen(wx.HORIZONTAL)
 
         # labels
         self.labelTitle1 = wx.StaticText(self.panel1, -1, "CRDS Software Loading Status", style=wx.ALIGN_CENTRE)
-        self.labelTitle1.SetFont(wx.Font(15, wx.DEFAULT, wx.NORMAL, wx.BOLD, 0, ""))
+        self.labelTitle1.SetFont(wx.Font(14, wx.DEFAULT, wx.NORMAL, wx.BOLD, 0, ""))
         self.labelFooter = wx.StaticText(self.panel3, -1, "Copyright Picarro, Inc. 1999-2010", style=wx.ALIGN_CENTER)
         
         # Current status display 
         self.curTextList = []        
         self.curCheckboxList = []
         for idx in range(len(self.taskList)):
-            staticText = wx.StaticText(self.panel2, -1, self.taskList[idx].split(".")[0], style=wx.TE_CENTRE)
+            try:
+                displayName = translationTable[self.taskList[idx]]
+            except:
+                displayName = self.taskList[idx].split(".")[0]
+            staticText = wx.StaticText(self.panel2, -1, displayName, style=wx.TE_CENTRE)
             staticText.SetFont(wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.NORMAL, 0, ""))
             self.curTextList.append(staticText)
             self.curCheckboxList.append((20,20))
@@ -83,7 +89,7 @@ class HostStartupFrame(wx.Frame):
         
         self.do_layout()
         
-    def do_layout(self, checkList = []):
+    def do_layout(self, checkList = [], idleList = []):
         logoSizer = wx.BoxSizer(wx.VERTICAL)
         logoSizer.Add(self.logoImage, 0, wx.TOP, 15)
         sizer_all = wx.BoxSizer(wx.VERTICAL)
@@ -100,15 +106,18 @@ class HostStartupFrame(wx.Frame):
         for idx in range(len(self.taskList)):
             grid_sizer_1.Add((50,-1))
             if idx in checkList:
-                #checkBmp = wx.Bitmap('./Check.ico', wx.BITMAP_TYPE_ICO)
                 checkBmp = wx.Bitmap(os.path.dirname(AppPath)+'/Check.png', wx.BITMAP_TYPE_PNG)
+                checkImage = wx.StaticBitmap(self.panel2, -1, checkBmp)
+                grid_sizer_1.Add(checkImage, 0, wx.ALL, 10)
+            elif idx in idleList:
+                checkBmp = wx.Bitmap(os.path.dirname(AppPath)+'/alarm.png', wx.BITMAP_TYPE_PNG)
                 checkImage = wx.StaticBitmap(self.panel2, -1, checkBmp)
                 grid_sizer_1.Add(checkImage, 0, wx.ALL, 10)
             else:
                 grid_sizer_1.Add(self.curCheckboxList[idx], 0, wx.ALL, 10)
                 
             grid_sizer_1.Add(self.curTextList[idx], 0, wx.ALL, 10)
-        sizer_2.Add(grid_sizer_1, 0, wx.EXPAND, 0)
+        sizer_2.Add(grid_sizer_1, 0, wx.EXPAND, wx.RIGHT|wx.LEFT, 20)
         self.panel2.SetSizer(sizer_2)
         
         sizer_3.Add(self.labelFooter, 0, wx.EXPAND|wx.TOP|wx.BOTTOM, 10)
@@ -129,34 +138,45 @@ class HostStartup(HostStartupFrame):
         fitterFound = False
         for appName in self.co["Applications"]:
             if appName == "RDFreqConverter":
-                taskList.append("RDFrequencyConverter.exe")
+                taskList.append("rdfrequencyconverter.exe")
             elif appName.startswith("Fitter"):
                 if not fitterFound:
-                    taskList.append("Fitter.exe")
+                    taskList.append("fitter.exe")
                     fitterFound = True
             else:
                 taskName = "%s.exe" % appName
-                taskList.append(taskName)
+                taskList.append(taskName.lower())
             if appName == "QuickGui":
                 break
+        taskList[3]="Test Module"
         HostStartupFrame.__init__(self, taskList, *args, **kwds)
-        self.taskList = taskList
         self.taskIdx = 0
         self.timer = wx.Timer(self)
         self.timer.Start(100)
         self.Bind(wx.EVT_TIMER, self.onTimer, self.timer)  
         self.startedList = []
+        self.idleList = []
+        self.startTime = time.time()
         
     def onTimer(self, event):
         if self.taskIdx == len(self.taskList):
             self.Destroy()
             return
         winProcessListStr = getWinProcessListStr()
-        if self.taskList[self.taskIdx].lower() in winProcessListStr:
+        if self.taskList[self.taskIdx] in winProcessListStr:
             self.curTextList[self.taskIdx].SetFont(wx.Font(12, wx.DEFAULT, wx.NORMAL, wx.BOLD, 0, ""))
             self.startedList.append(self.taskIdx)
-            self.do_layout(self.startedList)
+            self.do_layout(self.startedList, self.idleList)
+            self.startTime = time.time()
             self.taskIdx += 1
+        else:
+            if (time.time() - self.startTime) < appTimeout:
+                return
+            else:
+                self.idleList.append(self.taskIdx)
+                self.do_layout(self.startedList, self.idleList)
+                self.startTime = time.time()
+                self.taskIdx += 1
 
 HELP_STRING = \
 """
