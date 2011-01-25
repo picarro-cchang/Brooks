5bd4f5d0692fca740e58f7780e829ced335e7fb5
stan <stan@crds-d620-1>
2011-01-24 18:26:09 -0800

    [Ticket 203] Utility to cycle the power to the analyzer, allowing recovery from hardware errors.

---
+++ Utilities/Restart/ResetAnalyzer.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+#
+"""
+File Name: ResetAnalyzer.py
+Purpose: First step for cycling power to reset analyzer
+
+File History:
+    24-Jan-2011  sze  Initial version.
+
+Copyright (c) 2011 Picarro, Inc. All rights reserved
+"""
+
+from os import system, makedirs
+from os.path import abspath, exists, join, split 
+from _winreg import ConnectRegistry, SetValueEx, CloseKey, OpenKey
+from _winreg import KEY_WRITE, REG_SZ, HKEY_LOCAL_MACHINE
+from shutil import move
+from glob import glob
+from Host.Common.CMOS import CMOS
+
+def moveWildToDir(src,dest):
+    srcFiles = glob(src)
+    for f in srcFiles:
+        move(abspath(f),join(dest,split(f)[1]))
+    
+if __name__ == "__main__":
+    src  = r"C:\Documents and Settings\picarro\Start Menu\Programs\Startup"
+    dest = r"C:\Picarro\G2000\Log\DisabledStartup"
+    cmos = CMOS()
+    if cmos.getBiosName() != 'Phoenix - AwardBIOS v6.00PG':
+        raise ValueError('Unrecognized BIOS, cannot continue')
+    if not exists(dest): makedirs(dest)
+    moveWildToDir(src + "\\*", dest)
+    
+    year,month,day,hour,min,sec = cmos.getRTC()
+    # Advance by 5 min
+    min += 5
+    if min>=60:
+        min -= 60
+        hour += 1
+        if hour >= 24:
+            hour -= 24
+    cmos.setAlarmDay(0)
+    cmos.setAlarmHour(hour)
+    cmos.setAlarmMin(min)
+    cmos.setAlarmSec(sec)
+    cmos.setAlarmEnable(True)
+    print "Restarting at %2d:%02d:%02d" % (hour,min,sec)
+    aReg = ConnectRegistry(None,HKEY_LOCAL_MACHINE)
+    aKey = OpenKey(aReg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce", 0, KEY_WRITE)
+    try:   
+        SetValueEx(aKey,"MyNewKey",0, REG_SZ, r"c:\picarro\G2000\HostExe\RtcAlarmOff.exe") 
+    except EnvironmentError:                                          
+        print "Encountered problems writing into the Registry..."
+    CloseKey(aKey)
+    CloseKey(aReg)
+    system('shutdown -r -t 5 -c "Picarro analyzer reset (phase 1 of 3)"')
+    
---
+++ Utilities/Restart/Standalone/RtcAlarmOff.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#
+"""
+File Name: RtcAlarmOff.py
+Purpose: Turn off RTC alarm in BIOS which restarts computer at 
+    specified time
+
+File History:
+    24-Jan-2011  sze  Initial version.
+
+Copyright (c) 2011 Picarro, Inc. All rights reserved
+"""
+from os import system
+from _winreg import ConnectRegistry, SetValueEx, CloseKey, OpenKey
+from _winreg import KEY_WRITE, REG_SZ, HKEY_LOCAL_MACHINE
+from CMOS import CMOS
+    
+if __name__ == "__main__":
+    cmos = CMOS()
+    cmos.setAlarmEnable(False)    
+    aReg = ConnectRegistry(None,HKEY_LOCAL_MACHINE)
+    aKey = OpenKey(aReg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce", 0, KEY_WRITE)
+    try:   
+        SetValueEx(aKey,"MyNewKey",0, REG_SZ, r"c:\picarro\G2000\HostExe\restart3.bat") 
+    except EnvironmentError:                                          
+        print "Encountered problems writing into the Registry..."
+    CloseKey(aKey)
+    CloseKey(aReg)
+    system('shutdown -s -t 5 -c "Picarro analyzer reset (phase 2 of 3)"')
