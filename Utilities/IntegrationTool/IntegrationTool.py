c857b367a6b9a40d175b2c38f7d51354921ec57e
alee <>
2011-04-13 15:12:26 -0700

    Added MADS (HF) type

--- Utilities/IntegrationTool/IntegrationTool.py
+++ Utilities/IntegrationTool/IntegrationTool.py
@@ -33,7 +33,7 @@ HOTBOX_CAL = "Beta2000_HotBoxCal"
 
 LASER_TYPE_DICT = {"1603.2": "CO2", "1651.0": "CH4", "1599.6": "iCO2", "1392.0": "iH2O", 
                    "1567.9": "CO", "1527.0": "NH3", "1554.7": "iH2O", "1521.1": "C2H2",
-                   "1658.7": "CH4"}
+                   "1658.7": "CH4", "1278.0": "HF"}
 
 ANALYZER_TYPE_INDEX = {"HBDS":0, "CFADS":1, "CFBDS":1, "CFDDS":1, "CFEDS":1, "CFFDS":6, "CBDS":2, "CHADS":2, "CFKADS":3, "CFKBDS":3, "AEDS":4, "CKADS":5}
 ALL_SCHEMES = [
