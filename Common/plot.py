43d308fe6ed60ed94aa430bdcd86b51c00fe8a12
alee <>
2011-04-18 11:24:46 -0700

    Made the fix in plot.py more robust when determining the data set range is smaller than the python precision

--- Common/plot.py
+++ Common/plot.py
@@ -1488,7 +1488,7 @@ class PlotCanvas(wx.Panel):
                 return lower, upper
         elif spec == 'auto':
             range = upper-lower
-            if range < 10**-10:
+            if range < max(abs(upper), abs(lower))*(10**-10):
                 return lower-0.5, upper+0.5
             log = _Numeric.log10(range)
             power = _Numeric.floor(log)
