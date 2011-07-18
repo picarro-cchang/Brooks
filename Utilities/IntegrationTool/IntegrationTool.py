698238369636d3531f4659c70c24694b2610954f
alee <alee@alee-d410>
2011-07-18 16:35:17 -0700

    Added HIDS in IntegrationTool.py

--- Utilities/IntegrationTool/IntegrationTool.py
+++ Utilities/IntegrationTool/IntegrationTool.py
@@ -38,7 +38,7 @@ LASER_TYPE_DICT = {"1603.2": "CO2", "1651.0": "CH4", "1599.6": "iCO2", "1392.0":
                    "1567.9": "CO", "1527.0": "NH3", "1554.7": "iH2O", "1521.1": "C2H2",
                    "1658.7": "CH4", "1278.0": "HF"}
 
-ANALYZER_TYPE_INDEX = {"HBDS":0, "CFADS":1, "CFBDS":1, "CFHADS":7, "CFDDS":1, "CFEDS":1, "CFFDS":6, "CBDS":2, "CHADS":2, "CFKADS":3, "CFKBDS":3, "AEDS":4, "CKADS":5}
+ANALYZER_TYPE_INDEX = {"HBDS":0, "HIDS":0, "CFADS":1, "CFBDS":1, "CFHADS":7, "CFDDS":1, "CFEDS":1, "CFFDS":6, "CBDS":2, "CHADS":2, "CFKADS":3, "CFKBDS":3, "AEDS":4, "CKADS":5}
 ALL_SCHEMES = [
               { "O18"      : r"C:\Picarro\G2000\InstrConfig\Schemes\HBDSxx_Fixed_Peak18O.sch",
                 "Baseline" : r"C:\Picarro\G2000\InstrConfig\Schemes\HBDSxx_Baseline.sch",
