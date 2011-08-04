282ea7f414fc86018d9d811853ee3e0c42cd4b5d
stan@picarro.com <>
2011-08-03 23:01:21 -0700

    Change paths for compiling Cypress USB code.

--- CypressUSB/EZUSB_FX2/buildLarge.bat
+++ CypressUSB/EZUSB_FX2/buildLarge.bat
@@ -6,7 +6,7 @@ REM #
 REM #	Copyright (c) 2000 Cypress Semiconductor, Inc. All rights reserved
 REM #--------------------------------------------------------------------------
 
-set EZTARGET=C:\work\G2000\SrcCode\CypressUSB
+set EZTARGET=C:\Picarro\G2000\SrcCode\CypressUSB
 set C51INC=C:\Keil\C51\INC;%EZTARGET%\INC
 
 REM ### Compile code ###
