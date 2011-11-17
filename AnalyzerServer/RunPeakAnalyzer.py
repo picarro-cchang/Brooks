3f93cf009e9872bb486e580265a3edbc6700d57a
picarro <picarro@MI910-G2000a>
2011-11-17 10:55:05 -0800

    Add sparkline to web GUI and start peak analyzer for analyzing Keeling plot

--- AnalyzerServer/RunPeakAnalyzer.py
+++ AnalyzerServer/RunPeakAnalyzer.py
@@ -1,12 +1,12 @@
 import os
 import sys
-from PeakFinder import PeakFinder
+from PeakAnalyzer import PeakAnalyzer
 
 def getLocalAnalyzerId():
     try:
         import CmdFIFO
         RPC_PORT_DRIVER = 50010
-        CRDS_Driver = CmdFIFO.CmdFIFOServerProxy("http://localhost:%d" % RPC_PORT_DRIVER, ClientName = "RunPeakFinder")
+        CRDS_Driver = CmdFIFO.CmdFIFOServerProxy("http://localhost:%d" % RPC_PORT_DRIVER, ClientName = "RunPeakAnalyzer")
         curValDict = CRDS_Driver.fetchLogicEEPROM()[0]
         localAnalyzerId = curValDict["Analyzer"]+curValDict["AnalyzerNum"]
         print "Local analyzer id found: %s" % localAnalyzerId 
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         # Local
         analyzer = localAnalyzerId
         data_dir = os.path.join(AppDir,'static/datalog') # local Analyzer data location
-        debug = True
+        debug = False
     else:
         # P3
         if 1 < len(sys.argv):
@@ -38,7 +38,7 @@ if __name__ == "__main__":
             
         data_dir = os.path.join('/data/mdudata/datalogAdd', analyzer) # P3 Server data location
         
-        pidpath = os.path.join(data_dir, 'peakFinderMain.pid') 
+        pidpath = os.path.join(data_dir, 'peakAnalyzerMain.pid') 
         try:
             testf = open(pidpath, 'r')
             raise RuntimeError('pidfile exists. Verify that there is not another peakFinderMain task for the directory. path: %s.' % data_dir)
@@ -53,7 +53,7 @@ if __name__ == "__main__":
         pidf.close()
         
     ulog = os.path.join(data_dir, '*DataLog_User_Minimal.dat')
-    pf = PeakFinder(analyzerId=analyzer)
+    pf = PeakAnalyzer(analyzerId=analyzer)
     pf.userlogfiles = ulog
     pf.debug = debug
     pf.run()
