8bd168c9e2c2344e7e225e01c452dc93b57998e6
Sze Tan <stan@picarro.com>
2011-12-20 13:28:24 -0800

    Refactoring of KML generation routines, use a configuration file to specify active and inactive INI files for view server.

--- ViewServer/viewServer.py
+++ ViewServer/viewServer.py
@@ -14,11 +14,20 @@ import time
 import traceback
 import urllib
 
+if hasattr(sys, "frozen"): #we're running compiled with py2exe
+    appPath = sys.executable
+else:
+    appPath = sys.argv[0]
+appDir = os.path.split(appPath)[0]
+changeIni = os.path.join(appDir,'MobileKit.ini')
+baseIni = os.path.join(appDir,'MobileKit_inactive.ini') 
+
 # configuration
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
+DEFAULT_CONFIG_NAME = "viewServer.ini"
 
 viewParamsList = []
 viewParamsAsDir = {}
@@ -72,19 +81,10 @@ def updateFromConfig(changeIni):
         refreshNeeded = update('poly_color',str) | refreshNeeded
         if 'enabled' in settings:
             viewParams.enabled = int(settings['enabled'])
-    print "Refresh needed", refreshNeeded
     return refreshNeeded, settingsDict
     
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-if hasattr(sys, "frozen"): #we're running compiled with py2exe
-    appPath = sys.executable
-else:
-    appPath = sys.argv[0]
-appDir = os.path.split(appPath)[0]
-changeIni = os.path.join(appDir,'MobileKit.ini')
-baseIni = os.path.join(appDir,'MobileKit_inactive.ini') 
 
 class RestCallError(Exception):
     pass
@@ -117,32 +117,51 @@ def emptyResponse():
     response.headers['Content-Type'] = 'application/vnd.google-earth.kml+xml'
     return response
 
-@app.route('/base')
-def base():
-    blockStrings = []
-    for i,viewParams in enumerate(viewParamsList):
-        blockStrings.append("""<Folder id="multi%d">
-</Folder>
-<Style id="poly%d">
-    <LineStyle>
-        <color>%s</color>
-        <width>4</width>
-    </LineStyle>
-    <PolyStyle>
-        <color>%s</color>
-    </PolyStyle>
-</Style>""" % (i,i,viewParams.line_color,viewParams.poly_color))
+BLOCK = """<Folder id="multi%d">
+    <name>%s Concentration</name>
+    <Style id="poly%d">
+        <LineStyle>
+            <color>%s</color>
+            <width>4</width>
+        </LineStyle>
+        <PolyStyle>
+            <color>%s</color>
+        </PolyStyle>
+    </Style>
+</Folder>"""
 
-    kml = """<?xml version="1.0" encoding="UTF-8"?>
+BASE = """<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
     <Document id="baseDoc">
         <name>Select Concentrations to Display</name>
         %s
     </Document>
-</kml>""" % ("\n".join(blockStrings),)
+</kml>"""
+
+@app.route('/base')
+def base():
+    blockStrings = []
+    for i,viewParams in enumerate(viewParamsList):
+        blockStrings.append(BLOCK % (i,viewParams.speciesName,i,viewParams.line_color,viewParams.poly_color))
+
+    kml = BASE % ("\n".join(blockStrings),)
     response = make_response(kml)
     response.headers['Content-Type'] = 'application/vnd.google-earth.kml+xml'
     return response
+
+VIEW = """<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+<Document>
+    <LookAt>
+          <longitude>%s</longitude>
+          <latitude>%s</latitude>
+          <range>%s</range>
+          <tilt>%s</tilt>
+          <heading>%s</heading>
+          <altitude>%s</altitude>
+    </LookAt>
+</Document>
+</kml>"""
     
 @app.route('/updateView')
 def updateView():
@@ -160,26 +179,54 @@ def updateView():
         result = service.getData({'startPos':-2,'varList':'["GPS_ABS_LONG","GPS_ABS_LAT"]'})
         long = result['GPS_ABS_LONG'][-1]
         lat = result['GPS_ABS_LAT'][-1]
-        kml = """<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://www.opengis.net/kml/2.2">
-<Document>
-    <LookAt>
-          <longitude>%s</longitude>
-          <latitude>%s</latitude>
-          <range>%s</range>
-          <tilt>%s</tilt>
-          <heading>%s</heading>
-          <altitude>%s</altitude>
-    </LookAt>
-</Document>
-</kml>""" % (long,lat,range-altitude/cos(tilt*DTR),tilt,heading,altitude)
+        kml = VIEW % (long,lat,range-altitude/cos(tilt*DTR),tilt,heading,altitude)
         response = make_response(kml)
         response.headers['Content-Type'] = 'application/vnd.google-earth.kml+xml'
         return response
     except:
         print traceback.format_exc()
         return emptyResponse()
-        
+
+UPDATE = """<Delete>
+    %s
+</Delete>
+<Create>
+    <Document targetId="baseDoc">
+        <name>Select Concentrations to Display</name>
+        %s
+    </Document>
+</Create>"""
+
+PLACEMARK = """<Placemark>
+    <name>%s Concentration</name>
+    <styleUrl>#poly%d</styleUrl>
+    <LineString>
+        <extrude>1</extrude>
+        <tessellate>1</tessellate>
+        <altitudeMode>relativeToGround</altitudeMode>
+        <coordinates>
+            %s
+        </coordinates>
+     </LineString>
+</Placemark>"""
+
+CONCFOLDER = """<Folder targetId="multi%d">
+    <visibility>%s</visibility>
+    %s
+</Folder>"""
+
+NETLINKCONTROL = """<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+<NetworkLinkControl>
+    <Update>
+        <targetHref>http://localhost:5100/base</targetHref>
+        %s
+        %s
+    </Update>
+    <cookie>%s</cookie>
+</NetworkLinkControl>
+</kml>"""
+
 @app.route('/updateData')
 def updateData():
     cookie = {}
@@ -230,35 +277,11 @@ def updateData():
             delStrings = []
             createStrings = []
             for i,viewParams in enumerate(viewParamsList):
-                print "ViewParams %r,%r,%r" % (viewParams.speciesName,viewParams.line_color,viewParams.poly_color)
                 delStrings.append("""<Folder targetId="multi%d"></Folder>""" % (i,))
-                createStrings.append("""
-<Folder id="multi%d">
-    <name>%s Concentration</name>
-    <Style id="poly%d">
-        <LineStyle>
-            <color>%s</color>
-            <width>4</width>
-        </LineStyle>
-        <PolyStyle>
-            <color>%s</color>
-        </PolyStyle>
-    </Style>
-</Folder>""" % (i,viewParams.speciesName,i,viewParams.line_color,viewParams.poly_color))
-
-            kmlPrefix = """
-<Delete>
-    %s
-</Delete>
-<Create>
-    <Document targetId="baseDoc">
-        <name>Select Concentrations to Display</name>
-        %s
-    </Document>
-</Create>""" % ("\n".join(delStrings), "\n".join(createStrings))
+                createStrings.append(BLOCK % (i,viewParams.speciesName,i,viewParams.line_color,viewParams.poly_color))
+            kmlPrefix = UPDATE  % ("\n".join(delStrings), "\n".join(createStrings))
         else:
             cookie["lastPos"] = "%s" % lastPos
-            
             dataStrings = []
             
             for i,viewParams in enumerate(viewParamsList):
@@ -266,54 +289,40 @@ def updateData():
                 if len(lats)>1:
                     for conc,lat,long in zip(concs[i],lats,longs):
                         data[i].append("%.5f,%.5f,%.0f" % (long,lat,viewParams.scale*(conc-viewParams.offset)))
-                    placeMark = """
-<Placemark>
-    <name>%s Concentration</name>
-    <styleUrl>#poly%d</styleUrl>
-    <LineString>
-        <extrude>1</extrude>
-        <tessellate>1</tessellate>
-        <altitudeMode>relativeToGround</altitudeMode>
-        <coordinates>
-            %s
-        </coordinates>
-     </LineString>
-</Placemark>""" % (viewParams.speciesName,i,"\n".join(data[i]))
-                dataStrings.append("""
-<Folder targetId="multi%d">
-    <name>%s Concentration</name>
-    <visibility>%s</visibility>
-    %s
-</Folder>""" % (i,viewParams.speciesName,viewParams.enabled,placeMark))
-
+                    placeMark = PLACEMARK % (viewParams.speciesName,i,"\n".join(data[i]))
+                dataStrings.append(CONCFOLDER % (i,viewParams.enabled,placeMark))
                 kmlData = """<Create>%s</Create>""" % ("\n".join(dataStrings))
         # Convert the cookie dictionary into a string
         cookie = Markup.escape("&".join(["%s=%s" % (k,cookie[k]) for k in cookie]))
         
-        kml = """<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://www.opengis.net/kml/2.2">
-<NetworkLinkControl>
-    <Update>
-        <targetHref>http://localhost:5100/base</targetHref>
-        %s
-        %s
-    </Update>
-    <cookie>%s</cookie>
-</NetworkLinkControl>
-</kml>""" % (kmlPrefix, kmlData, cookie)
+        kml = NETLINKCONTROL  % (kmlPrefix, kmlData, cookie)
         response = make_response(kml)
         response.headers['Content-Type'] = 'application/vnd.google-earth.kml+xml'
         return response
     except:
         print traceback.format_exc()
         return emptyResponse()
+        
+HELP_STRING = \
+"""
+
+viewServer.py [-h] [-c <FILENAME>] [-a <IPADDR>]
+
+Where the options can be a combination of the following:
+-h, --help : Print this help.
+-c         : Specify a config file.
+-a         : Specify IP address fror analyzerServer
+"""
+
+def PrintUsage():
+    print HELP_STRING
+
     
-if __name__ == '__main__':
+def HandleCommandSwitches():
     import getopt
-    shortOpts = 'a:'
-    longOpts = []
+
     try:
-        switches, args = getopt.getopt(sys.argv[1:], shortOpts, longOpts)
+        switches, args = getopt.getopt(sys.argv[1:], "hc:a:", ["help"])
     except getopt.GetoptError, data:
         print "%s %r" % (data, data)
         sys.exit(1)
@@ -323,10 +332,33 @@ if __name__ == '__main__':
     for o, a in switches:
         options[o] = a
 
+    if "-h" in options or "--help" in options:
+        PrintUsage()
+        sys.exit()
+
+    #Start with option defaults...
+    configFile = os.path.dirname(appPath) + "/" + DEFAULT_CONFIG_NAME
+
+    if "-c" in options:
+        configFile = options["-c"]
+        print "Config file specified at command line: %s" % configFile
+        
+    return configFile, options
+
+if __name__ == '__main__':
+    configFile,options = HandleCommandSwitches()
+    configFile = os.path.abspath(configFile)
+    configPath = os.path.split(configFile)[0]
+    
     addr = '127.0.0.1'
     if "-a" in options:
         addr = options["-a"]
 
+    ini = ConfigObj(configFile)
+    if 'Main' in ini:
+        baseIni = os.path.join(configPath,ini['Main'].get('inactiveIniPath',baseIni))
+        changeIni = os.path.join(configPath,ini['Main'].get('activeIniPath',changeIni))
+    
     setupFromConfig(baseIni)
     
     # Connect to the analyzer rest server
