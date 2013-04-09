57e3d3482e9b014f1868ad24a04c5e589be8daaa
Sze Meng Tan <stan@picarro.com>
2013-04-09 14:55:30 -0700

    Checking in siteConfig.js

---
+++ MobileKit/NodeReportGen/lib/siteConfig.js
@@ -0,0 +1,9 @@
+/* siteConfig.js defines the object that stores site configuration data for the node report generation program. */
+/*global exports, module, require */
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+
+define(function(require, exports, module) {
+	'use strict';
+    module.exports = {
+    };
+});
