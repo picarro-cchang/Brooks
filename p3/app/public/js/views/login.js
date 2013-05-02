c4448751e0698e7b86ef1a669a6a93dd27d4fdd6
Joshua Montross <joshua.montross@gmail.com>
2013-05-02 10:47:02 -0700

    redirect after login to /investigator/front page, on front page, link to profile at /investigator/profile

--- p3/app/public/js/views/login.js
+++ p3/app/public/js/views/login.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 			}
 		},
 		success	: function(responseText, status, xhr, $form){
-			if (status == 'success') window.location.href = '/front_page';
+			if (status == 'success') window.location.href = '/investigator/front_page';
 		},
 		error : function(e){
             lv.showLoginError('Login Failure', 'Please check your username and/or password');
