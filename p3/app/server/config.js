d140eae923640d65068e3c3f9b5bbd7f63b040e7
Ben Huffman <1stdraft@gmail.com>
2013-05-03 16:23:03 -0700

    Updated prime handoff and css styling

--- p3/app/server/config.js
+++ p3/app/server/config.js
@@ -19,6 +19,15 @@ module.exports = function(){
             return {"mongo":{"dbport":37018}};
 
         default:
-            return {"mongo":{"dbport":37017}};
+            return {
+                "mongo":{"dbport":27017}
+               ,"p3node" : {
+                     "csp_url":  "https://localhost:8081/node" //"https://dev.picarro.com/node"
+                   , "ticket_url":  "https://localhost:8081/node/rest/sec/dummy/1.0/Admin/" //"https://dev.picarro.com/node/rest/sec/dummy/1.0/Admin/"
+                   , "identity":  "85490338d7412a6d31e99ef58bce5dPM"
+                   , "psys":  "SUPERADMIN"
+                   , "rprocs":  '["AnzMeta:byAnz", "AnzLogMeta:byEpoch", "AnzLog:byEpoch"]'
+                 }
+             };
     }
 };
