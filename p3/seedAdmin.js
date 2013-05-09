823f824359420bd62ad850af6dd18f7f2660c7b2
Joshua Montross <joshua.montross@gmail.com>
2013-05-09 10:31:11 -0700

    admin seed uses proper db since we changed it to login_investigator

--- p3/seedAdmin.js
+++ p3/seedAdmin.js
@@ -1,7 +1,7 @@
 
 
 var dblist = [
-              "node-login"
+              "login_investigator"
              ];
 
 var insdoc = { "name" : "awesomeAdmin", "email" : "admin@picarro.com", "bio" : "news", "experimentInfo" : "is good", "user" : "admin@picarro.com", "pass" : "wDbqR0WD9A8f8005805d89481a4c2b0228e4dfe601", "country" : "USA", "date" : "April 18th 2013, 4:38:14 pm", "anz" : "FDDS2015", "anzName" : "FDDS2015", "anzIdentity" : "2999bad153a4ee48da94836ad265764f", "admin": "true"}
