21e51ce693240e0e16d36199ea783ac802292d8b
Tracy Walder <twalder@picarro.com>
2013-09-23 20:00:57 -0700

    little_reader: replace easygui file open dialog with wx.FileOpen(), PEP-8 formatting

--- PlumeScanner/little_reader.py
+++ PlumeScanner/little_reader.py
@@ -19,7 +19,8 @@ Takes in pickled data file and unpickles it into
 9. GPS Long
 10. CO2
 """
-import easygui as eg
+
+import wx
 import cPickle
 import pdb
 import string
@@ -27,62 +28,66 @@ import pylab
 import numpy
 
 
-def printpretty (floatnum):
+def printpretty(floatnum):
     return string.ljust(str(floatnum), 12)
 
-def readfunc(filename, peakplot = 'y', savefileyn = 'y', outputvar = ""):
+
+def readfunc(filename, peakplot='y', savefileyn='y', outputvar=""):
     time = []
     ch4 = []
-    h2o =[]
+    h2o = []
     co2 = []
     carspeed = []
     windlat = []
-    gpslat =[]
+    gpslat = []
     gpslong = []
     stdwindir = []
     var = []
-    savefilename = filename.replace(".dat","_unpickled.txt")
+    savefilename = filename.replace(".dat", "_unpickled.txt")
     if savefileyn == 'y':
         f = open(savefilename, "w")
     else:
         pass
     try:
-        with open(filename,'rb') as fileo:
+        with open(filename, 'rb') as fileo:
             while True:
-                try: 
+                try:
                     obj = cPickle.load(fileo)
                     if obj["data"]["SpectrumID"] == 25:
                         if savefileyn == 'y':
-                            f.write(printpretty(obj["data"]["time"])+"\t"+ \
-                            printpretty(obj["data"]["CH4_dry"])+"\t"+ \
-                            printpretty(obj["data"]["H2O"])+"\t"+ \
-                            printpretty(obj["data"]["ValveMask"])+"\t"+ \
-                            printpretty(obj["data"]["SpectrumID"])+"\t")
+                            f.write(printpretty(obj["data"]["time"]) + "\t" +
+                                    printpretty(obj["data"]["CH4_dry"]) + "\t" +
+                                    printpretty(obj["data"]["H2O"]) + "\t" +
+                                    printpretty(obj["data"]["ValveMask"]) + "\t" +
+                                    printpretty(obj["data"]["SpectrumID"]) + "\t")
+
                             #print printpretty(obj["data"]["time"]), "\t", printpretty(obj["data"]["CH4_dry"])
                             time.append(obj["data"]["time"])
                             ch4.append(obj["data"]["CH4_dry"])
                             h2o.append(obj["data"]["H2O"])
+
                             try:
-                                f.write(printpretty(obj["data"]["WS_WIND_LAT"])+"\t"+ \
-                                printpretty(obj["data"]["WS_WIND_LON"])+"\t"+ \
-                                printpretty(obj["data"]["CAR_SPEED"])+"\t"+ \
-                                printpretty(obj["data"]["GPS_ABS_LAT"])+"\t" +\
-                                printpretty(obj["data"]["GPS_ABS_LONG"])+"\t")
+                                f.write(printpretty(obj["data"]["WS_WIND_LAT"]) + "\t" +
+                                        printpretty(obj["data"]["WS_WIND_LON"]) + "\t" +
+                                        printpretty(obj["data"]["CAR_SPEED"]) + "\t" +
+                                        printpretty(obj["data"]["GPS_ABS_LAT"]) + "\t" +
+                                        printpretty(obj["data"]["GPS_ABS_LONG"]) + "\t")
+
                                 carspeed.append(obj["data"]["CAR_SPEED"])
                                 windlat.append(obj["data"]["WS_WIND_LAT"])
                                 stdwindir.append(obj["data"]["WIND_DIR_SDEV"])
                                 gpslat.append(obj["data"]["GPS_ABS_LAT"])
                                 gpslong.append(obj["data"]["GPS_ABS_LONG"])
                                 try:
-                                    f.write(printpretty(obj["data"]["CO2"])+"\n")                            
+                                    f.write(printpretty(obj["data"]["CO2"])+"\n")
                                     co2.append(obj["data"]["CO2"])
                                     erroro = "End of file reached"
                                 except:
                                     f.write("\n")
                                     erroro = 'Missing CO2 data'
                             except:
-                                try:                             
-                                    f.write(printpretty(obj["data"]["CO2"])+"\n")                            
+                                try:
+                                    f.write(printpretty(obj["data"]["CO2"])+"\n")
                                     co2.append(obj["data"]["CO2"])
                                     erroro = "Missing Wind and Car speed data"
                                 except:
@@ -93,7 +98,7 @@ def readfunc(filename, peakplot = 'y', savefileyn = 'y', outputvar = ""):
                                 except:
                                     var.append('')
                         else:
-                            erroro = 'No output'                            
+                            erroro = 'No output'
                             time.append(obj["data"]["time"])
                             ch4.append(obj["data"]["CH4_dry"])
                             h2o.append(obj["data"]["H2O"])
@@ -105,8 +110,8 @@ def readfunc(filename, peakplot = 'y', savefileyn = 'y', outputvar = ""):
                                 var.append(obj["data"][outputvar])
                             except:
                                 var.append('')
-                        
-                except: 
+
+                except:
                     print erroro
                     if savefileyn == 'y':
                         f.close()
@@ -115,22 +120,22 @@ def readfunc(filename, peakplot = 'y', savefileyn = 'y', outputvar = ""):
                     #pdb.set_trace()
                     if peakplot == 'y':
                         pylab.figure("Concentrations")
-                        pylab.subplot(3,1,1)
+                        pylab.subplot(3, 1, 1)
                         pylab.plot(time, ch4)
                         pylab.title("Concentrations")
-                        pylab.ylabel ("CH4 (ppm)")
-                        pylab.subplot(3,1,2)
+                        pylab.ylabel("CH4 (ppm)")
+                        pylab.subplot(3, 1, 2)
                         pylab.plot(time, h2o)
                         pylab.ylabel("H2O")
-                        pylab.subplot(3,1,3)
+                        pylab.subplot(3, 1, 3)
                         pylab.plot(time, co2)
                         pylab.xlabel("Epoch Time")
                         pylab.ylabel("CO2")
-                        pylab.figure("Peripherals") #Assuming that the valves are off for first 50 pts
-                        pylab.subplot(2,1,1)
+                        pylab.figure("Peripherals")  # Assuming that the valves are off for first 50 pts
+                        pylab.subplot(2, 1, 1)
                         pylab.plot(carspeed[:50])
                         pylab.title("Car speed")
-                        pylab.subplot(2,1,2)
+                        pylab.subplot(2, 1, 2)
                         pylab.plot(windlat[:50])
                         pylab.title("Wind Lat")
                         pylab.figure("Std Wind Dev")
@@ -143,16 +148,27 @@ def readfunc(filename, peakplot = 'y', savefileyn = 'y', outputvar = ""):
     return savefilename, var
 
 if __name__ == "__main__":
-    filename = eg.fileopenbox(msg="File to unpickle") 
-    readfunc(filename, 'n','y', '')
-#    filename = r'C:\Users\ttsai\Desktop\Uintah Basin Field Campaign\EPA\for paper\batch\plume_1368624776_2013_5_15__6_32.dat'
-#    test = readfunc(filename,'n','n','WIND_DIR_SDEV')
-#    conc =readfunc(filename,'n','n','CH4_dry')
-#    speed = readfunc(filename,'n','n','CAR_SPEED')
-#    pylab.subplot(3,1,1)
-#    pylab.plot(conc[1][:50])
-#    pylab.subplot(3,1,2)
-#    pylab.plot(speed[1][:50])
-#    pylab.subplot(3,1,3)
-#    pylab.plot(test[1][:50])
-#    pylab.show()
+    app = wx.PySimpleApp()
+
+    d = wx.FileDialog(None, "File to unpickle",
+                      style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST,
+                      wildcard="All files (*.*)|*.*")
+
+    if d.ShowModal() == wx.ID_OK:
+        filename = d.GetPath()
+
+        readfunc(filename, 'n', 'y', '')
+
+    d.Destroy()
+
+    #    filename = r'C:\Users\ttsai\Desktop\Uintah Basin Field Campaign\EPA\for paper\batch\plume_1368624776_2013_5_15__6_32.dat'
+    #    test = readfunc(filename,'n','n','WIND_DIR_SDEV')
+    #    conc =readfunc(filename,'n','n','CH4_dry')
+    #    speed = readfunc(filename,'n','n','CAR_SPEED')
+    #    pylab.subplot(3,1,1)
+    #    pylab.plot(conc[1][:50])
+    #    pylab.subplot(3,1,2)
+    #    pylab.plot(speed[1][:50])
+    #    pylab.subplot(3,1,3)
+    #    pylab.plot(test[1][:50])
+    #    pylab.show()
