c592356daa5346bdae67b4f7201b2e12fd973f09
Tracy Walder <twalder@picarro.com>
2013-10-10 14:45:15 -0700

    Sze's original version for automated replacement of print statements in scripts with pass. Reason this is needed is that print calls have been determined to cause latency in the analyzer .

---
+++ Host/Utilities/ManageTemplates/getTemplates.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import subprocess
+
+dest = r"c:\temp\Templates"
+# Find all the bzr repositories under root
+root = r"s:\CrdsRepositoryNew\Releases\G2000\1.3\Config"
+for base, dirs, files in os.walk(root):
+    if '.bzr' in dirs:
+        # This is a bzr directory, find the portion 
+        #  which excludes root
+        destDir = os.path.join(dest, base[len(root)+1:])
+        try:
+            os.makedirs(destDir)
+        except: # Already exists
+            pass
+        os.chdir(destDir)
+        subprocess.call(["bzr", "branch", "--use-existing-dir", base, "."])
+    if base.endswith('.bzr'): del dirs
