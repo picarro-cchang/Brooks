8a4ddec64f052637ea73b759aef06917541bf22f
Tracy Walder <twalder@picarro.com>
2013-10-28 14:29:52 -0700

    PlotControlPanel.py: working test code for registering a listener for plot controls

--- Host/DatViewer/PlotControlPanel.py
+++ Host/DatViewer/PlotControlPanel.py
@@ -8,9 +8,16 @@ import wx
 ######################################
 # A simple flex grid for the controls.
 #
-class PlotControlPanelFlexGrid(wx.Panel):
+class PlotControlPanelGui(wx.Panel):
     def __init__(self, *args, **kwds):
-        kwds["style"] = wx.TAB_TRAVERSAL
+        kwds["style"] = wx.TAB_TRAVERSAL | wx.SUNKEN_BORDER
+
+        if "panelNum" in kwds:
+            self.panelNum = kwds["panelNum"]
+            del kwds["panelNum"]
+        else:
+            self.panelNum = 0
+
         wx.Panel.__init__(self, *args, **kwds)
 
         # Create the controls
@@ -112,6 +119,144 @@ class PlotControlPanelFlexGrid(wx.Panel):
         plotControlsSizer.SetSizeHints(self)
 
 
+class TestClientData(object):
+    def __init__(self, panelNum):
+        self.panelNum = panelNum
+
+    def GetPanelNum(self):
+        return self.panelNum
+
+
+class Subject(object):
+    def __init__(self, *a, **kwds):
+        self.listeners = {}
+        self.nextListenerIndex = 0
+
+    def addListener(self, listenerFunc):
+        self.listeners[self.nextListenerIndex] = listenerFunc
+        retVal = self.nextListenerIndex
+        self.nextListenerIndex += 1
+        return retVal
+
+    def removeListener(self, listenerIndex):
+        del self.listeners[listenerIndex]
+
+    def update(self):
+        for l in self.listeners:
+            self.listeners[l](self)
+
+
+"""
+class PlotDisplayPanel(PlotDisplayPanelGui):
+     def __init__(self, *a, **kwds):
+        PlotDisplayPanelGui.__init__(self, *a, **kwds)
+        self.model = Subject()
+"""
+
+
+class PlotControlPanel(PlotControlPanelGui):
+    def __init__(self, *a, **kwds):
+        PlotControlPanelGui.__init__(self, *a, **kwds)
+        self.model = Subject()
+
+        # pull settings for autoscale Y and show points from prefs
+        fAutoscaleY = False
+        fShowPoints = True
+
+        self.model.settings = {"fAutoscaleY": fAutoscaleY,
+                               "fShowPoints": fShowPoints,
+                               "strDataSetName": "",
+                               "strVarName": ""}
+
+        self.model.changed = ""
+
+        dataSetNameList = ["", "data A", "data B", "data C"]
+        self.dataSetNameChoice.SetItems(dataSetNameList)
+        self.dataSetNameChoice.SetSelection(0)
+
+        varNameList = ["", "var1", "var2", "var3"]
+        self.varNameChoice.SetItems(varNameList)
+        self.varNameChoice.SetSelection(0)
+
+        self.autoscaleYChk.SetValue(fAutoscaleY)
+        self.showPointsChk.SetValue(fShowPoints)
+
+        self.Bind(wx.EVT_CHOICE, self.OnChooseVar, self.varNameChoice)
+        self.Bind(wx.EVT_CHOICE, self.OnChooseDataSet, self.dataSetNameChoice)
+
+        self.Bind(wx.EVT_CHECKBOX, self.OnAutoscaleYChk, self.autoscaleYChk)
+        self.Bind(wx.EVT_CHECKBOX, self.OnShowPointsChk, self.showPointsChk)
+
+        self.Bind(wx.EVT_BUTTON, self.OnNAvgCalculate, self.calcNAvgBtn)
+
+    def OnChooseVar(self, event):
+        print "Selected var changed"
+        #print "event=", event
+        #print dir(event)
+        ix = self.varNameChoice.GetSelection()
+        varName = self.varNameChoice.GetStringSelection()
+        print "  ix=%d  varName=%s" % (ix, varName)
+
+        # Note: wx returns Unicode strings here, we might need to
+        #       convert them to ASCII
+        self.model.settings["strVarName"] = varName
+        self.model.changed = "strVarName"
+        self.model.update()
+
+    def OnChooseDataSet(self, event):
+        print "Selected data set changed"
+        #print "event=", event
+        #print dir(event)
+        ix = self.dataSetNameChoice.GetSelection()
+        dataSetName = self.dataSetNameChoice.GetStringSelection()
+        print "  ix=%d  dataSetName=%s" % (ix, dataSetName)
+
+        self.model.settings["strDataSetName"] = dataSetName
+        self.model.changed = "strDataSetName"
+        self.model.update()
+
+    def OnAutoscaleYChk(self, event):
+        print "Autoscale Y checkbox clicked"
+        #print "event=", event
+        #print dir(event)
+        self.model.settings["fAutoscaleY"] = self.autoscaleYChk.GetValue()
+        self.model.changed = "fAutoscaleY"
+        self.model.update()
+
+    def OnShowPointsChk(self, event):
+        print "Show points checkbox clicked"
+        #print "event=", event
+        #print dir(event)
+        self.model.settings["fShowPoints"] = self.showPointsChk.GetValue()
+        self.model.changed = "fShowPoints"
+        self.model.update()
+
+    def OnNAvgCalculate(self, event):
+        print "Calculate average button clicked"
+        self.model.changed = "nAvgCalculate"
+        self.model.update()
+
+
+class ListenerWrapper(object):
+    def __init__(self, index):
+        self.index = index
+
+    def plotControlsListener(self, model):
+        print "plotControlsListener:"
+        print "  index=", self.index
+        print "  settings=", model.settings
+        print "  changed=", model.changed
+
+        """
+        for key in model.settings:
+            value = model.settings[key]
+            print "  key=", key
+            print "  value type =", type(value)
+        """
+
+        print ""
+
+
 class TestFrame(wx.Frame):
     def __init__(self, nPanels=1):
         wx.Frame.__init__(self, None, -1, "Plot Control Frame")
@@ -119,67 +264,68 @@ class TestFrame(wx.Frame):
         # TODO: pass whether to include the N average controls in the panel
         # fIncludeNavg = True
         self.panels = []
+        self.listeners = []
 
-        for ix in range(nPanels+1):
-            panel = PlotControlPanelFlexGrid(self, wx.ID_ANY, style=wx.RAISED_BORDER)
+        # Sizer to hold all of the plot controls
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+
+        for ix in range(nPanels):
+            panel = PlotControlPanel(self, wx.ID_ANY,
+                                     style=wx.RAISED_BORDER,
+                                     panelNum=ix)
             self.panels.append(panel)
 
-            # populate dropdowns and init other controls in the panel
+            listenerWrapper = ListenerWrapper(ix)
 
-            # InsertItems worked on my Macbook Pro but not on Windows -- WTF??
-            # On Windows it's SetItems.
-            dataSetNameList = ["data A", "data B", "data C"]
-            #panel.dataSetNameChoice.InsertItems(dataSetNameList, 0)
-            panel.dataSetNameChoice.SetItems(dataSetNameList)
-            panel.dataSetNameChoice.SetSelection(0)
+            ixListener = panel.model.addListener(listenerWrapper.plotControlsListener)
+            self.listeners.append(ixListener)
 
-            # useful for debugging -- print out the function names available
-            """
-            print "type(panel.dataSetNameChoice)=", type(panel.dataSetNameChoice)
-            choiceFcns = dir(panel.dataSetNameChoice)
-            print choiceFcns
-
-            with open("ChoiceFcns.txt", 'w') as fp:
-                for f in choiceFcns:
-                    print >>fp, f
-            """
-
-            varNameList = ["var1", "var2", "var3"]
-            #panel.varNameChoice.InsertItems(varNameList, 0)
-            panel.varNameChoice.SetItems(varNameList)
-            panel.varNameChoice.SetSelection(0)
-
-            # SetString() is for replacing an existing choice
-            #panel.varNameChoice.SetString(0, "replaced var1")
-
-            self.Bind(wx.EVT_CHOICE, self.OnChooseVar, panel.varNameChoice)
-
-            # check the Autoscale Y checkbox and bind
-            panel.autoscaleYChk.SetValue(True)
-            self.Bind(wx.EVT_CHECKBOX, self.OnAutoscaleYChk, panel.autoscaleYChk)
-
-            # init a text box value
-            panel.meanText.SetValue("1.0")
-
-            self.Bind(wx.EVT_BUTTON, self.OnCalcAverage, panel.calcNAvgBtn)
-
-            # wrap a sizer around the panel
+            # wrap a sizer around this panel
             sizer = wx.BoxSizer(wx.VERTICAL)
             sizer.Add(panel, 0, wx.LEFT | wx.TOP | wx.RIGHT | wx.BOTTOM, 15)
-            self.SetSizer(sizer)
+            #self.SetSizer(sizer)
+            #panel.SetSizer(sizer)
 
+            # add this panel sizer to the main sizer
+            mainSizer.Add(sizer, 0, wx.TOP | wx.BOTTOM, 10)
+
+        # fit the frame to the needs of the main sizer
+        self.SetSizer(mainSizer)
+        mainSizer.Fit(self)
+
+    """
     def OnCalcAverage(self, event):
         print "Calculate average button clicked"
+        print "event=", event
+        print dir(event)
+
+        tcd = event.GetClientData()
+        print "tcd=", tcd
+        #print "panelNum=", tcd.GetPanelNum()
 
     def OnAutoscaleYChk(self, event):
         print "Autoscale Y checkbox clicked"
-        print "State=", self.panel.autoscaleYChk.GetValue()
+        print "event=", event
+        print dir(event)
+
+        tcd = event.GetClientData()
+        print "tcd=", tcd
+
+        co = event.GetClientObject()
+        print "co=", co
+        #print "State=", self.panel.autoscaleYChk.GetValue()
 
     def OnChooseVar(self, event):
         print "Selected var changed"
+        print "event=", event
+        print dir(event)
+        print ""
+        tcd = event.GetClientData()
+        print "tcd=", tcd
         ix = self.panel.varNameChoice.GetSelection()
         varName = self.panel.varNameChoice.GetStringSelection()
         print "  ix=%d  varName=%s" % (ix, varName)
+    """
 
 
 class App(wx.App):
@@ -187,7 +333,7 @@ class App(wx.App):
         wx.App.__init__(self, redirect, filename)
 
     def OnInit(self):
-        self.frame = TestFrame()
+        self.frame = TestFrame(nPanels=3)
         self.frame.Show()
         self.SetTopWindow(self.frame)
         return True
