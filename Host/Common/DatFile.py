7231a721e64cd66c059798fefb62afe6d0c3ba64
Patrick J. Franz <pfranz@picarro.com>
2013-11-06 16:57:48 -0800

    Soil post-processor includes ChemDetect on plots now.

--- Host/Common/DatFile.py
+++ Host/Common/DatFile.py
@@ -13,7 +13,7 @@ import csv
 class DatFile(object):
 
     @staticmethod
-    def _load(fname):
+    def _load(fname, allowedCols):
         """
         Returns a dict-like object of named data streams. All data
         points are stored as strings. It is the caller's
@@ -25,22 +25,25 @@ class DatFile(object):
         with open(fname, 'r') as fp:
             lines = fp.readlines()
             for i, col in enumerate(lines[0].split()):
-                data[col] = []
                 names[i] = col
+
+                if allowedCols is None or col in allowedCols:
+                    data[col] = []
 
             for line in lines[1:]:
                 for i, v in enumerate(line.split()):
-                    data[names[i]].append(v)
+                    if allowedCols is None or names[i] in allowedCols:
+                        data[names[i]].append(v)
 
         return data
 
-    def __init__(self, fname):
+    def __init__(self, fname, allowedCols):
         """
         Load and parse the specified .dat file.
         """
 
         self.fname = fname
-        self.data = DatFile._load(self.fname)
+        self.data = DatFile._load(self.fname, allowedCols)
 
     def __getattr__(self, stream):
         """
