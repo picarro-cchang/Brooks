0199458c0a975f3f2b46a4583c36bdbd40f8c6e1
Tracy Walder <twalder@picarro.com>
2013-11-11 20:00:42 -0800

    remove duplicated command line from makeFitutils27.bat

--- Host/Fitter/makeFitutils27.bat
+++ Host/Fitter/makeFitutils27.bat
@@ -1,4 +1,2 @@
 python c:\python27\scripts\f2py.py -c -m fitutils fitutils.f --compiler=mingw32
 python setup.py build_src build_ext --inplace -c mingw32
-
-python setup.py build_src build_ext --inplace -c mingw32
