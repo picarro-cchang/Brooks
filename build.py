31d767ff1af7b67e28fe98d959b7f91cdab96b07
Sze Tan <stan@picarro.com>
2015-06-24 10:13:32 -0700

    Call doit build_hostexe from pyb_. Get version information from JSON file and set up dist directory for PyBuilder

--- build.py
+++ build.py
@@ -1,4 +1,6 @@
+import json
 import os
+import pprint
 
 from pybuilder.core import after, before, use_plugin, init, task
 from pybuilder.errors import BuildFailedException
@@ -13,13 +15,45 @@ use_plugin("python.install_dependencies")
 # use_plugin("python.flake8")
 # use_plugin("python.coverage")
 
-name = "Host"
 default_task = "publish"
 
 @init
-def set_properties(project):
-    pass
+def initialize(project, logger):
+    official = project.get_property("official", False)
+    product = project.get_property("product", "g2000")
+    
+    version_file = os.path.join("versions","%s_version.json" % product)
+    
+    with open(version_file,"r") as inp:
+        ver = json.load(inp)
+        project.name = product
+        project.version = ("%s.%s.%s.%s%s" % 
+            (ver["major"], ver["minor"], ver["revision"], ver["build"], "" if official else "-INTERNAL" ))
+    # Need to set the directory into which the output distribution is placed        
+    project.set_property('dir_dist','$dir_target/dist/%s-%s' % (project.name, project.version))
 
+@after("prepare")
+def setup_buildenv(project, logger):
+    official = project.get_property("official", False)
+    product = project.get_property("product", "g2000")
+
+    reports_dir = project.expand_path("$dir_reports")
+    if not os.path.exists(reports_dir):
+        os.mkdir(reports_dir)
+    output_file_path = os.path.join(reports_dir, "setup_buildenv")
+
+    logger.info("Setting up build environment")
+    cmd = ["doit", "make_release", "--product", product]
+    if official:
+        cmd.append(["--official"])
+    with open(output_file_path, "w") as output_file:
+        process = subprocess.Popen(cmd,
+                                   stdout=output_file,
+                                   stderr=output_file,
+                                   shell=False)
+        return_code = process.wait()
+        if return_code != 0:
+            raise BuildFailedException("Error while executing make_release")
 def write_setup_script(project, logger):
     setup_script = project.expand_path("$dir_dist/Host/setup.py")
     logger.info("Writing setup.py as %s", setup_script)
@@ -51,14 +85,13 @@ def build_binary_distribution(project, logger):
             raise BuildFailedException(
                 "Error while executing setup command %s, see %s for details" % (command, output_file_path))
     shutil.move(project.expand_path("$dir_dist/Host/dist"),project.expand_path("$dir_dist/HostExe"))
-
 @before("publish")
 def run_py2exe(project, logger):
     logger.info("Running py2exe in %s", project.expand_path("$dir_dist"))
-    reports_dir = project.expand_path("$dir_reports/run_py2exe")
+    reports_dir = project.expand_path("$dir_reports")
     if not os.path.exists(reports_dir):
         os.mkdir(reports_dir)
-    output_file_path = os.path.join(reports_dir, "hostexe")
+    output_file_path = os.path.join(reports_dir, "build_hostexe")
     with open(output_file_path, "w") as output_file:
         process = subprocess.Popen(["doit", "build_hostexe", "--build_dir", "%s" % project.expand_path("$dir_dist/Host")],
                                    stdout=output_file,
@@ -69,6 +102,7 @@ def run_py2exe(project, logger):
             raise BuildFailedException("Error while executing run_py2exe/build_hostexe")
 
     shutil.move(project.expand_path("$dir_dist/Host/dist"),project.expand_path("$dir_dist/HostExe"))
+    
     
 @task
 def compile_sources(project, logger):
