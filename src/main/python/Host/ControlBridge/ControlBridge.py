a9a0942d1b1d19ee302691538b7bb8f870641515
Yuan Ren <yren@yren-850G1M.Corp.Picarro.Com>
2016-04-01 09:28:44 -0700

    Add ControlBridge and DataManagerPublisher

---
+++ src/main/python/Host/ControlBridge/ControlBridge.py
@@ -0,0 +1,126 @@
+"""
+Copyright 2014, Picarro Inc.
+"""
+
+import time
+import threading
+
+import zmq
+
+from Host.Common import SharedTypes
+from Host.Common import EventManagerProxy as EventManager
+from Host.Common import CmdFIFO
+
+EventManager.EventManagerProxy_Init('ControlBridge')
+
+class ControlBridge(object):
+
+    RESPONSE_STATUS = {
+        "success" : 0,
+        "unknown" : 1
+    }
+
+    # Previously defined in the gdu.js REST call to AnalyzerServer
+    INJECT_VALVE_BIT = 2
+    INJECT_VALVE_MASK = 1 << INJECT_VALVE_BIT
+    INJECT_FLAG_VALVE_BIT = 3
+    INJECT_FLAG_VALVE_MASK = 1 << INJECT_FLAG_VALVE_BIT
+    INJECT_MASK = INJECT_VALVE_MASK | INJECT_FLAG_VALVE_MASK
+    INJECT_SAMPLES = 5
+    EXTRA_FLAG_SAMPLES = 50 # each is 0.2 seconds
+
+    def __init__(self):
+        self.driver = CmdFIFO.CmdFIFOServerProxy("http://localhost:%d" % SharedTypes.RPC_PORT_DRIVER,
+                                                 ClientName = "ControlBridge")
+        EventManager.Log('Connected to Driver')
+        self.instMgr = CmdFIFO.CmdFIFOServerProxy("http://localhost:%d" % SharedTypes.RPC_PORT_INSTR_MANAGER,
+                                                  ClientName = "ControlBridge")
+        EventManager.Log('Connected to Instrument Manager')
+
+        self.context = zmq.Context()
+
+        self.controlSocket = self.context.socket(zmq.REP)
+        self.controlSocket.bind("tcp://127.0.0.1:%d" % SharedTypes.TCP_PORT_CONTROL_BRIDGE_ZMQ)
+
+        self.commands = {
+            "armIsotopicCapture" : self._armIsotopicCapture,
+            "shutdown" : self._shutdown,
+            "cancelIsotopicAnalysis" : self._cancelIsotopicAnalysis,
+            "getPeakDetectorState" : self._getPeakDetectorState,
+            "setToIdle" : self._setToIdle,
+            "referenceGasPrime" : self._referenceGasPrime,
+            "referenceGasInjection" : self._referenceGasInjection,
+            "setToEQMode": self._setToEQMode,
+            "setToSurveyorMode": self._setToSurveyorMode
+        }
+ 
+    def run(self):
+        try:
+            while True:
+                cmd = self.controlSocket.recv_string()
+                ret = None
+
+                try:
+                    ret = self.commands[cmd]()
+                    response = ControlBridge.RESPONSE_STATUS["success"]
+
+                except KeyError:
+                    response = ControlBridge.RESPONSE_STATUS["unknown"]
+
+                self.controlSocket.send_string("%d,%s" % (response, ret))
+
+        finally:
+            self.controlSocket.close()
+            self.context.term()
+
+    def _getPeakDetectorState(self):
+        return self.driver.rdDasReg("PEAK_DETECT_CNTRL_STATE_REGISTER")
+
+    def _armIsotopicCapture(self):
+        self.driver.wrDasReg("PEAK_DETECT_CNTRL_STATE_REGISTER", 1)
+
+    def _setToIdle(self):
+        self.driver.wrDasReg("PEAK_DETECT_CNTRL_STATE_REGISTER", 0)
+
+    def _shutdown(self):
+        self.instMgr.INSTMGR_ShutdownRpc(0)
+
+    def _cancelIsotopicAnalysis(self):
+        self.driver.wrDasReg("PEAK_DETECT_CNTRL_STATE_REGISTER", 5)
+
+        doneCount = 0
+
+        while True:
+            if doneCount == 600:
+                break
+
+            val = self.driver.rdDasReg('PEAK_DETECT_CNTRL_STATE_REGISTER')
+            EventManager.Log("Waiting for peak detector to return to Idle: %s" % val)
+
+            if val == 0:
+                doneCount += 1
+
+            time.sleep(0.010)
+
+    def _referenceGasPrime(self):
+        self.driver.wrDasReg("PEAK_DETECT_CNTRL_STATE_REGISTER", 6)
+
+    def _referenceGasInjection(self):
+        self.driver.wrValveSequence([
+            [ControlBridge.INJECT_MASK, ControlBridge.INJECT_MASK, ControlBridge.INJECT_SAMPLES],
+            [ControlBridge.INJECT_MASK, ControlBridge.INJECT_FLAG_VALVE_MASK, ControlBridge.EXTRA_FLAG_SAMPLES],
+            [ControlBridge.INJECT_MASK, 0, 1],
+            [0, 0, 0]
+        ])
+        
+        self.driver.wrDasReg("VALVE_CNTRL_SEQUENCE_STEP_REGISTER", 0)
+
+    def _setToEQMode(self):
+        self.driver.closeValves(0x20)   # close valve 6
+        
+    def _setToSurveyorMode(self):
+        self.driver.openValves(0x20)   # open valve 6
+        
+if __name__ == '__main__':
+    bridge = ControlBridge()
+    bridge.run()
---
+++ src/main/python/Host/DataManager/DataManagerPublisher.py
@@ -0,0 +1,81 @@
+"""
+Copyright 2014, Picarro Inc.
+"""
+
+import calendar
+import time
+
+import zmq
+import json
+
+import Queue
+from Host.Common import Listener
+from Host.Common import SharedTypes
+from Host.Common import StringPickler
+from Host.Common import AppStatus
+from Host.Common import EventManagerProxy as EventManager
+
+EventManager.EventManagerProxy_Init('DataManagerPublisher')
+
+class DataManagerPublisher(object):
+
+    def __init__(self):
+        self.context = zmq.Context()
+
+        self.broadcastSocket = self.context.socket(zmq.PUB)
+        self.broadcastSocket.bind("tcp://127.0.0.1:%d" % SharedTypes.TCP_PORT_DATAMANAGER_ZMQ_PUB)
+
+        self.queue = Queue.Queue()
+        self.listener = Listener.Listener(self.queue, SharedTypes.BROADCAST_PORT_DATA_MANAGER,
+                                          StringPickler.ArbitraryObject,
+                                          streamFilter=self.jsonifyMeasurements, retry=True)
+        #self.instrumentStatusListener = Listener.Listener(self.queue, SharedTypes.STATUS_PORT_INST_MANAGER,
+        #                                                  AppStatus.STREAM_Status,
+        #                                                  streamFilter=self.jsonifyStatus, retry=True)
+
+    def run(self):
+        try:
+            while True:
+                msgs = self.queue.get()
+                for m in msgs:
+                    #print m
+                    self.broadcastSocket.send_string("%s" % m)
+
+        finally:
+            self.broadcastSocket.close()
+            self.context.term()
+            self.listener.stop()
+
+    def jsonifyMeasurements(self, entry):
+        source = entry['source']
+        if source == 'Sensors' or source == 'analyze_FBDS':
+            if 'species' in entry['data'] and (entry['data']['species'] == 0.0 or entry['data']['species'] == "0.0"):
+                print "Rejected species 0"
+                return None
+            elif source == 'Sensors' and entry['mode'] == 'FBDS_mode':
+                # block these data during the measurement, otherwise non-cavity parameters will be treated as 0 by surveyor programs
+                # CH4 and flow values shown on tablet will jump between 0 and normal reading.
+                return None
+            return [{'type': 'measurement'}, {'mode': entry['mode']}, json.dumps(entry['data'])]
+        elif source == 'parseGPS' or source == 'parseGillAnemometer':
+            entry['data']['EPOCH_TIME'] = entry['time']
+            return [{'type': 'gps' if source == 'parseGPS' else 'anemometer' }, json.dumps(entry['data'])]
+        else:
+            #print "Skip %s" % entry['source']
+            return None
+
+    def jsonifyStatus(self, entry):
+        # Status messages do not contain timestamps!
+        epochTime = calendar.timegm(time.gmtime())
+
+        if self.lastInstrumentStatus == entry.status:
+            return None
+
+        result = json.dumps({'type': 'inst_status', 'data': {'timestamp': epochTime, 'status': entry.status}})
+        print result
+        self.lastInstrumentStatus = entry.status
+        return result
+
+if __name__ == "__main__":
+    pub = DataManagerPublisher()
+    pub.run()
