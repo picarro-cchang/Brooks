c3c0d753480e1455f1005308acca16133ee7212e
Wenting Tan <wtan@picarro.com>
2016-04-25 09:53:28 -0700

    change the default mode. The current default mode is FEDS(not EQ) mode.

--- src/main/python/Host/ControlBridge/ControlBridge.py
+++ src/main/python/Host/ControlBridge/ControlBridge.py
@@ -115,11 +115,13 @@ class ControlBridge(object):
         
         self.driver.wrDasReg("VALVE_CNTRL_SEQUENCE_STEP_REGISTER", 0)
 
+	def _setToSurveyorMode(self):
+        self.driver.openValves(0x20)   # open valve 6
+		
     def _setToEQMode(self):
         self.driver.closeValves(0x20)   # close valve 6
         
-    def _setToSurveyorMode(self):
-        self.driver.openValves(0x20)   # open valve 6
+    
         
 if __name__ == '__main__':
     bridge = ControlBridge()
