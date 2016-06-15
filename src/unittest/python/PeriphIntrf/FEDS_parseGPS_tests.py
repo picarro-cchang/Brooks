3cb022c4b0adab3358cf9ba0033b4e59dc974507
Yuan Ren <yren@yren-850G1M.Corp.Picarro.Com>
2016-06-14 20:48:27 -0700

    Add unit test for GPS parsing script

--- src/unittest/python/PeriphIntrf/FEDS_parseGPS_tests.py
+++ src/unittest/python/PeriphIntrf/FEDS_parseGPS_tests.py
@@ -28,6 +28,10 @@ class TestRegularGPS(unittest.TestCase):
         self.assertTrue(len(gpsData) == 0)
         gpsData = self.tester.runScript("$GPGST,012345.67,,,,,1.0,2.0")
         self.assertTrue(gpsData[2:4] == [1.0, 2.0])
+        gpsData = self.tester.runScript("$GPGGA,012345.98,12.345,N,123.456,W,1")
+        self.assertTrue(len(gpsData) == 0)
+        gpsData = self.tester.runScript("$GPGST,012345.98,,,,,1.0,2.0")
+        self.assertTrue(gpsData[2:4] == [1.0, 2.0])
         
     def test_iGPS_not_initialized(self):
         self.tester.dataLabels = ["GPS_ABS_LAT", "GPS_ABS_LONG", "GPS_UNC_LAT", "GPS_UNC_LONG", "GPS_FIT", "GPS_TIME"]
