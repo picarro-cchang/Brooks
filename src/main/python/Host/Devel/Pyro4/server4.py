073f30f416284f5608ef8e3bce658ff3d96eda54
Sze Tan <stan@picarro.com>
2016-08-27 15:19:08 -0700

    Update CmdFIFO to use Pyro4 @expose

--- src/main/python/Host/Devel/Pyro4/server4.py
+++ src/main/python/Host/Devel/Pyro4/server4.py
@@ -4,6 +4,7 @@ import os
 import Pyro4
 import time
 
+@Pyro4.expose
 class JokeGen(object):
     def joke(self, name):
         print "joke starts at ", time.time()
@@ -18,9 +19,9 @@ def main():
         Pyro4.config.SERVERTYPE="thread"
     else:
         Pyro4.config.SERVERTYPE="multiplex"
-    daemon = Pyro4.core.Daemon(port=7766)    
+    daemon = Pyro4.core.Daemon(port=7766)
     uri1=daemon.register(JokeGen(), objectId="jokegen")   # let Pyro create a unique name for this one
-    
+
     print("JokeGen is ready, not using the Name Server.")
     print(uri1)
     daemon.requestLoop()
