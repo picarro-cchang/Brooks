9dadc9f618a786e214c167fe6d3d69d5c321b3f1
Ray <rfellers@picarro.com>
2016-10-06 10:28:22 -0700

    Change dir creation to use '/' and not '\' on Linux.

--- src/main/python/Host/DataLogger/DataLogger.py
+++ src/main/python/Host/DataLogger/DataLogger.py
@@ -265,7 +265,10 @@ class DataLog(object):
         self.PrintTimeInHalfSecond = ConfigParser.getboolean(self.LogName, "printTimeInHalfSecond", False)
         self.WriteEpochTime = ConfigParser.getboolean(self.LogName, "writeEpochTime", True)
         self.WriteJulianDays = ConfigParser.getboolean(self.LogName, "writeJulianDays", True)
-        relDir = "%s\%s" % (ConfigParser.get(self.LogName, "srcfolder"),self.LogName)
+        if sys.platform == "win32":
+            relDir = "%s\%s" % (ConfigParser.get(self.LogName, "srcfolder"),self.LogName)
+        else:
+            relDir = "%s/%s" % (ConfigParser.get(self.LogName, "srcfolder"),self.LogName)
         self.srcDir = os.path.join(basePath, relDir)
         if self.liveArchive and self.useHdf5:
             raise ValueError('Cannot use live archive with HDF5 files in %s' % LogName)
