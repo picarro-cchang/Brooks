45229fb19b70d38e9dad50f54201a9fbc8965df6
Raymond Fellers <rfellers@picarro.com>
2017-04-15 13:39:25 -0700

    Improve the look of the QuickGui plot.
    Changed the line and marker color. Made the markers slightly larger squares with no
    fill.  The QuickGui is not passing all the marker attributes from the ini so I had
    to hardcode some settings in the plotting code to get it to work.

--- src/main/python/Host/Common/plot.py
+++ src/main/python/Host/Common/plot.py
@@ -278,6 +278,16 @@ class PolyMarker(PolyPoints):
                 - 'plus'
         """
 
+        # This is a kludge to bypass a bug to big to fix.
+        # The QuickGui doesn't pass the marker attribute settings in the QuickGui.ini
+        # properly. It ties together the line and marker attributes so that you can't do
+        # things like have different colors for lines and markers.
+        #
+        # I want blue lines and black markers so here I override any passed in
+        # attribute and force the marker to be black and transparent.
+        attr["colour"] = 'black'
+        attr["fillstyle"] = wx.TRANSPARENT
+
         PolyPoints.__init__(self, points, attr)
 
     def draw(self, dc, printerScale, coord= None):
