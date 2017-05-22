89599ef5fa816a5ce7d2e61ad7267a8c3023b591
Raymond Fellers <rfellers@picarro.com>
2017-05-22 01:04:38 -0700

    Hongbing's working SADS changes from SADS2018.

--- src/main/python/Host/Coordinator/Coordinator.py
+++ src/main/python/Host/Coordinator/Coordinator.py
@@ -355,9 +355,9 @@ class CoordinatorFrame(CoordinatorFrameGui):
 
     def makeFilename(self, fileType = "save"):
         if fileType == "log":
-            (dirName, baseName) = os.path.split(self.config.get("Files", "log", "/CoordinatorData/Log/"))
+            (dirName, baseName) = os.path.split(self.config.get("Files", "log", "/home/picarro/SI2000/Log/CoordinatorData/Log/"))
         else:
-            (dirName, baseName) = os.path.split(self.config.get("Files", "output", "/CoordinatorData/"))
+            (dirName, baseName) = os.path.split(self.config.get("Files", "output", "/home/picarro/SI2000/Log/CoordinatorData/"))
         if not os.path.isdir(dirName):
             os.makedirs(dirName)
         if self.fileTime == "local":
