a95216b9185c11510db534e18714b018192d0078
Raymond Fellers <rfellers@picarro.com>
2018-04-03 13:04:13 -0700

    I2-730, enable close button when aborting.

--- src/main/python/Host/CalibrationValidationManager/CalibrationValidationManager.py
+++ src/main/python/Host/CalibrationValidationManager/CalibrationValidationManager.py
@@ -46,6 +46,7 @@ class Window(QtGui.QMainWindow):
         self.tm.job_aborted_signal.connect(self.taskWizardWidget.job_aborted)
         self.taskWizardWidget.start_run_signal.connect(partial( self.tableWidget.disable_edit, True))
         self.taskWizardWidget.abort_signal.connect(partial(self.tableWidget.disable_edit, False))
+        self.taskWizardWidget.abort_signal.connect(partial(self.closeBtn.setEnabled, True))
         self.taskWizardWidget.job_complete_signal.connect(partial(self.closeBtn.setEnabled, True))
         self.taskWizardWidget.start_run_signal.connect(self.start_running_tasks)
         self.taskWizardWidget.next_signal.connect(self.tm.next_subtask_signal)
