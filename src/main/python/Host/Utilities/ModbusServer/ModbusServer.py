9f853aad32a7d14d5c63d2501e327c8377a5a976
picarro-cvala <cvala@picarro.com>
2018-04-23 11:29:34 -0700

    Revert "Fixed problem of not logging exception in to event log"
    This reverts commit 57166bd725cf093c5bd64d7105184bad66531a84.

--- src/main/python/Host/Utilities/ModbusServer/ModbusServer.py
+++ src/main/python/Host/Utilities/ModbusServer/ModbusServer.py
@@ -179,7 +179,6 @@ class ModbusServer(object):
             # control thread gets command from queue and execute
             self.control_thread = threading.Thread(target=self.controller)
             self.control_thread.daemon = True
-            self.write_readonly_constant_data()
         except:
             LogExc("Unable to Start Modbus Server")
             time.sleep(1)
@@ -512,6 +511,8 @@ class ModbusServer(object):
             return 0.0
             
     def controller(self):
+        # Some time LogExc does not find reference in thread so importing in thread
+        from Host.Common.EventManagerProxy import LogExc
         try:
             addr_func_map = {}
             for v in self.register_variables[COIL-1]['variables']:
@@ -526,8 +527,11 @@ class ModbusServer(object):
             LogExc("Unable to Start Modbus Server properly, %s" %ex.message)
                 
     def data_writer(self):
+        # Some time LogExc does not find reference in thread so importing in thread 
+        from Host.Common.EventManagerProxy import LogExc
         try:
             time.sleep(1)
+            self.write_readonly_constant_data()
             while True:
                 time.sleep(0.1)
                 while not self.data_queue.empty():
