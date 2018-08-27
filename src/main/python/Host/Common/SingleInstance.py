d2c15dd7e3eeb1a90ae0768257460a8436c1de09
gsornsen <gsornsen@picarro.com>
2018-08-27 13:02:03 -0700

    Removed Windows code, re-factored for PEP-8 Compliance (minus variable name cases), removed dead code comments

--- src/main/python/Host/Common/SingleInstance.py
+++ src/main/python/Host/Common/SingleInstance.py
@@ -4,74 +4,50 @@
 # Purpose: Cavity Ringdown Instrument Driver for Silverstone DAS
 #
 # Notes:
-# Uses a Mutex to limit application to a single instance
-#
-# File History:
-# 06-10-17 sze   Created file (modified from Python Cookbook)
-# 08-03-12 sze   Added linux support
+# Uses a pid file to limit application to a single instance
 
-import sys
+import commands
+import os
 
-if sys.platform == "win32":
-    from win32event import CreateMutex
-    from win32api import GetLastError
-    from winerror import ERROR_ALREADY_EXISTS
 
-    class SingleInstance:
-        """ Limits application to single instance """
-
-        def __init__(self,name):
-            self.mutexname = name
-            self.mutex = CreateMutex(None, False, self.mutexname)
-            self.lasterror = GetLastError()
-
-        def alreadyrunning(self):
-            return (self.lasterror == ERROR_ALREADY_EXISTS)
-
-elif sys.platform == "linux2":
-    import commands
-    import os
-    class SingleInstance:
-        """ Limits application to single instance """
-        # The recommended place to put pid files is /run.  This location is
-        # preferred because early in the boot process this directory is cleared
-        # out so we don't have stale pid files hanging about after a power
-        # failure.
-        #
-        # /run is owned by root. User level pid files go into /run/user/<uid>.
-        # pid files should have the .pid extension.  Add it if it's not in the
-        # name.
-        #
-        def __init__(self,name):
-            if "pid" not in name:
-                name = name + ".pid"
-            pidFilePath = "/run/user/" + str(os.getuid()) + "/"
-            self.name = pidFilePath + name
-            if os.path.exists(self.name):
-                #pid = open(self.name,"r").read().strip()
-                with open(self.name, "r") as pid:
-                    pid.read().strip()
-                pidRunning = commands.getoutput('ls /proc | grep %s' % pid)
-                if pidRunning:
-                    self.lasterror = True
-                else:
-                    self.lasterror = False
+class SingleInstance:
+    """ Limits application to single instance """
+    # The recommended place to put pid files is /run.  This location is
+    # preferred because early in the boot process this directory is cleared
+    # out so we don't have stale pid files hanging about after a power
+    # failure.
+    #
+    # /run is owned by root. User level pid files go into /run/user/<uid>.
+    # pid files should have the .pid extension.  Add it if it's not in the
+    # name.
+    #
+    def __init__(self, name):
+        if "pid" not in name:
+            name = name + ".pid"
+        pidFilePath = "/run/user/" + str(os.getuid()) + "/"
+        self.name = pidFilePath + name
+        if os.path.exists(self.name):
+            with open(self.name, "r") as pid:
+                pid.read().strip()
+            pidRunning = commands.getoutput('ls /proc | grep %s' % pid)
+            if pidRunning:
+                self.lasterror = True
             else:
                 self.lasterror = False
-            if not self.lasterror:
-                #fp = open(self.name,'w')
-                #fp.write(str(os.getpid()))
-                #fp.close()
-                with open(self.name, "w") as fp:
-                    fp.write(str(os.getpid()))
+        else:
+            self.lasterror = False
+        if not self.lasterror:
+            with open(self.name, "w") as fp:
+                fp.write(str(os.getpid()))
 
-        def alreadyrunning(self):
-            return self.lasterror
+    def alreadyrunning(self):
+        return self.lasterror
 
-        def __del__(self):
-            if not self.lasterror:
-                if os.path.exists(self.name):
-                    os.unlink(self.name)
+    def __del__(self):
+        if not self.lasterror:
+            if os.path.exists(self.name):
+                os.unlink(self.name)
+
 
 if __name__ == "__main__":
     myapp = SingleInstance("Example")
@@ -79,4 +55,4 @@ if __name__ == "__main__":
         raise RuntimeError("Example is already running")
     else:
         print "Example running, press <Enter> to terminate"
-        raw_input();
+        raw_input()
