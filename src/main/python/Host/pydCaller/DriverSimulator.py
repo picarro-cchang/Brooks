e9877e2d1848ee660be61bae8d2a92206a4737fd
gsornsen <gsornsen@picarro.com>
2018-09-17 10:59:42 -0700

    Improper indent on docstring, does not impact anything, purely visual

--- src/main/python/Host/pydCaller/DriverSimulator.py
+++ src/main/python/Host/pydCaller/DriverSimulator.py
@@ -6,7 +6,7 @@ EventManagerProxy_Init(APP_NAME)
 
 if __name__ == "__main__":
     """
-        Import main from APP_NAME. This ensures our processes launch with
-        the same method regardless of called directly or via this pydCaller
-        """
+    Import main from APP_NAME. This ensures our processes launch with
+    the same method regardless of called directly or via this pydCaller
+    """
     main()
