e83155354fb2ca99d17b749b42d17254f37caa23
picarro-cvala <cvala@picarro.com>
2018-10-18 14:38:19 -0700

    Export added new envirnment varibles

--- Config/AMADS/bin/launchBinAMADS_DCRDS.sh
+++ Config/AMADS/bin/launchBinAMADS_DCRDS.sh
@@ -5,6 +5,10 @@
 # This also sets the search path correctly for anaconda and
 # Picarro python modules.
 # 
+
+export PICARRO_CONF_DIR=/home/picarro/I2000
+export PICARRO_LOG_DIR=/home/picarro/I2000/Log
+export EVENT_MANAGER_PROXY_MIN_MS_INTERVAL = 0
 export PATH=/home/picarro/anaconda2/bin:$PATH
 cd /home/picarro/I2000/Host/pydCaller/
 python -O Supervisor.py -c ../../AppConfig/Config/Supervisor/supervisorEXE_AMADS_DCRDS_LCT.ini
