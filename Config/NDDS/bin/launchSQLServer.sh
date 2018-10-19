a198d4a9729116f39cb5427fe9b94ed627283144
picarro-cvala <cvala@picarro.com>
2018-10-19 10:13:36 -0700

    Added new envirnment variables

--- Config/NDDS/bin/launchSQLServer.sh
+++ Config/NDDS/bin/launchSQLServer.sh
@@ -5,6 +5,9 @@
 # This also sets the search path correctly for anaconda and
 # Picarro python modules.
 # 
+export PICARRO_CONF_DIR=/home/picarro/I2000
+export PICARRO_LOG_DIR=/home/picarro/I2000/Log
+export EVENT_MANAGER_PROXY_MIN_MS_INTERVAL=0
 export PATH=/home/picarro/anaconda2/bin:$PATH
 export PYTHONPATH=/home/picarro/I2000:$PYTHONPATH
 cd /home/picarro/I2000/Host/WebServer/
--- Config/SADS/bin/launchBinSADS.sh
+++ Config/SADS/bin/launchBinSADS.sh
@@ -8,7 +8,7 @@
 
 export PICARRO_CONF_DIR=/home/picarro/I2000
 export PICARRO_LOG_DIR=/home/picarro/I2000/Log
-export EVENT_MANAGER_PROXY_MIN_MS_INTERVAL = 0
+export EVENT_MANAGER_PROXY_MIN_MS_INTERVAL=0
 export PATH=/home/picarro/anaconda2/bin:$PATH
 cd /home/picarro/I2000/Host/pydCaller/
 python -O Supervisor.py -c ../../AppConfig/Config/Supervisor/supervisorEXE_SADS_140Torr.ini
