b5050a911c3c0eb6b00b33fef0b88fadddda1450
picarro-cvala <cvala@picarro.com>
2019-03-27 16:18:38 -0700

    Added slave id selection as drop down between slave id 1 to 51

--- experiments/ModbusSettings/picarro-modbus-settings/src/components/ModbusPanelEditor.tsx
+++ experiments/ModbusSettings/picarro-modbus-settings/src/components/ModbusPanelEditor.tsx
@@ -13,28 +13,55 @@ export class ModbusPanelEditor extends PureComponent<
 > {
   labelWidth = 8;
 
-  onMinValueChange = ({ target }) =>
+  onSalveIdChange = ({ target }) =>
     this.props.onChange({ ...this.props.options, slaveId: target.value });
 
-  onMaxValueChange = ({ target }) =>
+  onTCPPortChange = ({ target }) =>
     this.props.onChange({ ...this.props.options, tcpPort: target.value });
+
+  validatePort = ({ target }) => {
+    if (target.value < 1024 || target.value > 65564) {
+      alert('Invalide port value, port value has to be between 1024 and 65564');
+      return;
+    }
+    this.onTCPPortChange(target);
+  };
 
   render() {
     const { options } = this.props;
     const { slaveId, tcpPort } = options;
 
+    let slaveIdSelection = [];
+    for (let i = 1; i < 52; i++) {
+      if (slaveId === i) {
+        slaveIdSelection.push(
+          <option value={i} selected>
+            {slaveId}
+          </option>
+        );
+      } else {
+        slaveIdSelection.push(<option value={i}>{i}</option>);
+      }
+    }
     return (
-      <PanelOptionsGroup title="Simple options">
+      <PanelOptionsGroup title="Modbus Settings">
+        <div className="gf-form">
+          <label className="gf-form-label width-8">Salve ID</label>
+          <div className="gf-form-select-wrapper max-width-12">
+            <select
+              className="input-small gf-form-input"
+              ng-change="ctrl.render()"
+              onChange={this.onSalveIdChange}
+            >
+              {slaveIdSelection}
+            </select>
+          </div>
+        </div>
+
         <FormField
-          label="Min value"
+          label="TCP Port"
           labelWidth={this.labelWidth}
-          onChange={this.onMinValueChange}
-          value={slaveId}
-        />
-        <FormField
-          label="Max value"
-          labelWidth={this.labelWidth}
-          onChange={this.onMaxValueChange}
+          onChange={this.onTCPPortChange}
           value={tcpPort}
         />
       </PanelOptionsGroup>
