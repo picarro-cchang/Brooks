68d9536cd2202f284be222a6f93ac0714e1a593a
picarro-cvala <cvala@picarro.com>
2019-04-04 13:12:15 -0700

    Used home environment variable to go to home location

--- experiments/Scripts/grafana/Demo_April2019/grafanaluanch.sh
+++ experiments/Scripts/grafana/Demo_April2019/grafanaluanch.sh
@@ -1,3 +1,3 @@
 cd ${HOME}/go/src/github.com/grafana/grafana/bin/linux-amd64/
-./grafana-server -homepath /home/picarro/go/src/github.com/grafana/grafana/
+./grafana-server -homepath ${HOME}/go/src/github.com/grafana/grafana/
 
