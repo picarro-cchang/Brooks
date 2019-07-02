c0553931cdb3108827c7eb8563aa2e0df0948bfe
gsornsen <gsornsen@picarro.com>
2019-07-02 10:47:10 -0700

    Added microseconds to timestamp for devices that poll faster than 1 Hz

--- experiments/common/timeutils.py
+++ experiments/common/timeutils.py
@@ -4,12 +4,12 @@ from datetime import datetime
 def get_local_timestamp():
     # Get a current local timestamp in IS0 8601 format
     # Example: '2019-06-27 10:40:34'
-    local_iso_8601 = datetime.now().replace(microsecond=0).isoformat(' ')
+    local_iso_8601 = datetime.now().isoformat(' ')
     return local_iso_8601
 
 
 def get_utc_timestamp():
     # Get a current utc timestamp in ISO 8601 format
     # Example: '2019-06-27 17:40:34'
-    utc_iso_8601 = datetime.utcnow().replace(microsecond=0).isoformat(' ')
+    utc_iso_8601 = datetime.utcnow().isoformat(' ')
     return utc_iso_8601
