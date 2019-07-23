72197b014462d6baf89fff329ff4388da8d6a724
gsornsen <gsornsen@picarro.com>
2019-07-22 17:46:16 -0700

    Added epoch timestamp to timeutils.py

--- experiments/common/timeutils.py
+++ experiments/common/timeutils.py
@@ -13,3 +13,9 @@ def get_utc_timestamp():
     # Example: '2019-06-27 17:40:34'
     utc_iso_8601 = datetime.utcnow().isoformat(' ')
     return utc_iso_8601
+
+
+def get_epoch_timestamp():
+    # Get a current epoch timestamp
+    epoch_time = datetime.timestamp(datetime.now())
+    return epoch_time
