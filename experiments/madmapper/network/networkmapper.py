b1f04c4e2dfd328d5068ebf4aedf2d740d0fe8aa
gsornsen <gsornsen@picarro.com>
2019-07-24 10:41:44 -0700

    Modified for PEP8 compliance

--- experiments/madmapper/network/networkmapper.py
+++ experiments/madmapper/network/networkmapper.py
@@ -6,11 +6,11 @@ from conf import network_prefix, host_port
 
 class NetworkMapper(object):
 
-    def get_ip_addresses(self):
+    @staticmethod
+    def get_ip_addresses():
         ip_addresses = subprocess.getoutput('hostname -I')
         ip_list = ip_addresses.split()
         return ip_list
-
 
     def get_rack_lan_ip(self, ip_addresses):
         ip = None
@@ -19,7 +19,6 @@ class NetworkMapper(object):
                 ip = ip_addresses[ip]
                 break
         return ip
-
 
     def is_picarro_host(self, host, port=50010, timeout=0.1):
         s = socket.socket()
@@ -37,7 +36,6 @@ class NetworkMapper(object):
                 print(f'{host} is Picarro Instrument: {result}')
         return result
 
-
     def ping_host(self, host):
         ping = subprocess.Popen(['ping', '-c', '1', '-W', '1', f'{host}'],
                                 stderr=subprocess.PIPE,
@@ -49,14 +47,14 @@ class NetworkMapper(object):
         else:
             return False
 
-
     def get_all_active_hosts(self):
         current = start = 2
         end = 255
         hosts = []
-        ip_addresses = [subprocess.Popen(['ping', '-c', '1', '-W', '1', f'{network_prefix}{x}'],
-                                stderr=subprocess.PIPE,
-                                stdout=subprocess.PIPE) for x in range(start, end)]
+        ip_addresses = [subprocess.Popen(
+            ['ping', '-c', '1', '-W', '1', f'{network_prefix}{x}'],
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE) for x in range(start, end)]
         for ip in ip_addresses:
             ip.communicate()
             return_code = ip.returncode
@@ -64,24 +62,27 @@ class NetworkMapper(object):
                 hosts.append(f'{network_prefix}{current}')
             current += 1
         if __debug__:
-            print(f'\nActive hosts: {hosts}\nTotal hosts up: {len(hosts)}\nHosts scanned: {end - start}\n')
+            print(f'\nActive hosts: {hosts}'
+                  f'Total hosts up: {len(hosts)}'
+                  f'Hosts scanned: {end - start}\n')
         return hosts
-
 
     def get_all_picarro_hosts(self):
         picarro_hosts = {}
-        verified_hosts = []
         active_hosts = self.get_all_active_hosts()
         rack_ip = self.get_rack_lan_ip(self.get_ip_addresses())
         if rack_ip in active_hosts:
             active_hosts.remove(rack_ip)
         for ip_address in active_hosts:
-            if self.is_picarro_host(ip_address) == True:
-                driver = CmdFIFO.CmdFIFOServerProxy(f'http://{ip_address}:{host_port}',
-            ClientName='NetworkMapper')
+            if self.is_picarro_host(ip_address) is True:
+                driver = CmdFIFO.CmdFIFOServerProxy(
+                    f'http://{ip_address}:{host_port}',
+                    ClientName='NetworkMapper')
             try:
                 instrument_dict = driver.fetchLogicEEPROM()[0]
-                serial_number = f'{instrument_dict.get("Chassis")}-{instrument_dict.get("Analyzer")}{instrument_dict.get("AnalyzerNum")}'
+                serial_number = f'{instrument_dict.get("Chassis")}-' \
+                    f'{instrument_dict.get("Analyzer")}' \
+                    f'{instrument_dict.get("AnalyzerNum")}'
                 if not picarro_hosts.get(serial_number):
                     picarro_hosts.update({f'{serial_number}': f'{ip_address}'})
             except Exception as e:
@@ -96,6 +97,3 @@ if __name__ == '__main__':
     with FunctionTimer(time_unit='s'):
         test = NetworkMapper()
         test.get_all_picarro_hosts()
-
-
-
