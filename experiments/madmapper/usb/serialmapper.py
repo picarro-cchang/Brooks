ebb21851c5ee068006b6f4478956b35250888d00
gsornsen <gsornsen@picarro.com>
2019-07-31 14:04:22 -0700

    Testing yapf pre-commit

--- experiments/madmapper/usb/serialmapper.py
+++ experiments/madmapper/usb/serialmapper.py
@@ -21,40 +21,37 @@ class SerialMapper(object):
         self.mfc_count = 0
         self.piglet_count = 0
         context = pyudev.Context()
-        for device in context.list_devices(subsystem='tty',
-                                           ID_BUS='usb'):
+        for device in context.list_devices(subsystem='tty', ID_BUS='usb'):
             self.logger.debug(f'Device found: {dict(device)}')
             if 'Numato' in device.get('ID_SERIAL'):
                 serial_interface = SerialInterface()
-                serial_interface.config(
-                    port=device.get('DEVNAME'),
-                    baudrate=19200
-                )
+                serial_interface.config(port=device.get('DEVNAME'),
+                                        baudrate=19200)
                 try:
                     serial_interface.open()
                     time.sleep(0.5)
                     serial_interface.write('id get\r')
                     time.sleep(0.25)
-                    numato_id = int(f'{serial_interface.read()}'
-                                    .split('\n')[1][-1])
+                    numato_id = int(
+                        f'{serial_interface.read()}'.split('\n')[1][-1])
                     serial_interface.close()
                     relay_rpc_port = self.relay_port + numato_id
                     self.devices['Serial_Devices'].update({
-                        f"{device.get('DEVNAME')}":
-                            {'Driver': 'NumatoDriver',
-                             'Path': device.get('DEVNAME'),
-                             'Baudrate': 19200,
-                             'Numato_ID': numato_id,
-                             'RPC_Port': relay_rpc_port}})
+                        f"{device.get('DEVNAME')}": {
+                            'Driver': 'NumatoDriver',
+                            'Path': device.get('DEVNAME'),
+                            'Baudrate': 19200,
+                            'Numato_ID': numato_id,
+                            'RPC_Port': relay_rpc_port
+                        }
+                    })
                     self.relay_count += 1
                 except Exception as e:
                     self.logger.critical(f'Unhandled Exception: {e}')
             elif 'Mega 2560 R3' in device.get('ID_MODEL_FROM_DATABASE'):
                 serial_interface = SerialInterface()
-                serial_interface.config(
-                    port=device.get('DEVNAME'),
-                    baudrate=38400
-                )
+                serial_interface.config(port=device.get('DEVNAME'),
+                                        baudrate=38400)
                 try:
                     serial_interface.open()
                     time.sleep(2)
@@ -64,25 +61,30 @@ class SerialMapper(object):
                     serial_interface.close()
                     piglet_rpc_port = self.piglet_port + (slot_id - 1)
                     self.devices['Serial_Devices'].update({
-                        f'{device.get("DEVNAME")}':
-                            {'Driver': 'PigletDriver',
-                             'Bank_ID': slot_id,
-                             'Topaz_A_HW_Rev': 'Null',
-                             'Topaz_B_HW_Rev': 'Null',
-                             'Whitfield_HW_Rev': 'Null',
-                             'Path': device.get('DEVNAME'),
-                             'Baudrate': 38400,
-                             'RPC_Port': piglet_rpc_port}})
+                        f'{device.get("DEVNAME")}': {
+                            'Driver': 'PigletDriver',
+                            'Bank_ID': slot_id,
+                            'Topaz_A_HW_Rev': 'Null',
+                            'Topaz_B_HW_Rev': 'Null',
+                            'Whitfield_HW_Rev': 'Null',
+                            'Path': device.get('DEVNAME'),
+                            'Baudrate': 38400,
+                            'RPC_Port': piglet_rpc_port
+                        }
+                    })
                     self.piglet_count += 1
                 except Exception as e:
                     self.logger.critical(f'Unhandled Exception: {e}')
             elif 'FTDI_FT232R_USB_UART_AL04J9KM' in device.get('ID_SERIAL'):
                 mfc_rpc_port = self.mfc_port + self.mfc_count
                 self.devices['Serial_Devices'].update({
-                    f'{device.get("DEVNAME")}': {'Driver': 'AlicatDriver',
-                                                 'Path': device.get('DEVNAME'),
-                                                 'Baudrate': 19200,
-                                                 'RPC_Port': mfc_rpc_port}})
+                    f'{device.get("DEVNAME")}': {
+                        'Driver': 'AlicatDriver',
+                        'Path': device.get('DEVNAME'),
+                        'Baudrate': 19200,
+                        'RPC_Port': mfc_rpc_port
+                    }
+                })
                 self.mfc_count += 1
         self.logger.debug(f'USB Serial Devices: {self.devices}')
         return self.devices
@@ -91,5 +93,6 @@ class SerialMapper(object):
 if __name__ == '__main__':
     from host.experiments.common.function_timer import FunctionTimer
     with FunctionTimer():
+        print(f'SerialMapper Started...')
         test = SerialMapper()
         test.get_usb_serial_devices()
