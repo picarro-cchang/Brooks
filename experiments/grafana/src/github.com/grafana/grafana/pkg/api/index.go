b193abe63aaedcb6b61d1d1147c5d165bda4dca6
gsornsen <gsornsen@gmail.com>
2019-08-03 23:03:49 -0700

    More de-badging and removal of web-dependent links

--- experiments/grafana/src/github.com/grafana/grafana/pkg/api/index.go
+++ experiments/grafana/src/github.com/grafana/grafana/pkg/api/index.go
@@ -324,15 +324,13 @@ func (hs *HTTPServer) setIndexViewData(c *m.ReqContext) (*dtos.IndexViewData, er
 
 	data.NavTree = append(data.NavTree, &dtos.NavLink{
 		Text:         "Help",
-		SubTitle:     fmt.Sprintf(`%s v%s (%s)`, setting.ApplicationName, setting.BuildVersion, setting.BuildCommit),
+		SubTitle:     fmt.Sprintf(`%s v%s`, setting.ApplicationName, setting.BuildVersion),
 		Id:           "help",
 		Url:          "#",
 		Icon:         "gicon gicon-question",
 		HideFromMenu: true,
 		Children: []*dtos.NavLink{
 			{Text: "Keyboard shortcuts", Url: "/shortcuts", Icon: "fa fa-fw fa-keyboard-o", Target: "_self"},
-			{Text: "Community site", Url: "http://community.grafana.com", Icon: "fa fa-fw fa-comment", Target: "_blank"},
-			{Text: "Documentation", Url: "http://docs.grafana.org", Icon: "fa fa-fw fa-file", Target: "_blank"},
 		},
 	})
 
