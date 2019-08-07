204769ed9d60cedabb999f0bda425e87670b67ca
epeatfield@picarro.com <epeatfield@picarro.com>
2019-08-07 15:09:55 -0700

    Minor change to files

--- experiments/grafanaApi/make_api_key.py
+++
@@ -1,8 +0,0 @@
-from grafana_api.grafana_api import GrafanaAPI
-
-auth = ("admin", "admin")
-api = GrafanaAPI(auth, host='localhost:3000')
-r = api.POST('/auth/keys', json={"role": "Admin", "name": "new_api_key"})
-print(r)
-r = api.GET('/auth/keys')
-print(r)
