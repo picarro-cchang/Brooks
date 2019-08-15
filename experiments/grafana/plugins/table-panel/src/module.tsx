3bc6b4d16a66036b098ca4e27ad3e491bf0e7a2a
epeatfield@picarro.com <epeatfield@picarro.com>
2019-08-15 11:59:09 -0700

    UI changes to table panel

---
+++ experiments/grafana/plugins/table-panel/src/module.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+import { PanelPlugin } from '@grafana/ui';
+// import { TablePanel } from './components/TablePanel';
+// import { TablePanelEditor } from './components/TablePanelEditor';
+
+//import {  HelloOptions } from './types';
+
+export const plugin = new PanelPlugin<Options>(TablePanel);
+
+// plugin.setEditor(TablePanelEditor);
