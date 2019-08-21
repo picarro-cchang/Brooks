26aa67e07725f5963193cd8437215aa9890d7ae9
epeatfield@picarro.com <epeatfield@picarro.com>
2019-08-21 11:09:23 -0700

    Much better look for Edit Panel

--- experiments/grafana/plugins/grafana-multi-panel/src/panels/StateMachine/components/EditPanel.tsx
+++ experiments/grafana/plugins/grafana-multi-panel/src/panels/StateMachine/components/EditPanel.tsx
@@ -1,5 +1,7 @@
-import React, {Component, PureComponent} from 'react';
+import React, {Component, PureComponent, ReactText} from 'react';
 import { EditPanelOptions } from "../types";
+import ReactList from 'react-list';
+
 
 
 class EditPanel extends PureComponent<EditPanelOptions> {
@@ -7,57 +9,127 @@ class EditPanel extends PureComponent<EditPanelOptions> {
     //Make Rows based on Banks Available
     //ex: Bank Panel 1 then input box to change name
     banks: any;
-    render() {
+
+    makeEditRow = () => {
         let edit_list = [];
 
-      //  const test = this.props.uistatus;
+        //  const test = this.props.uistatus;
         this.banks = this.props.uistatus.bank;
-
-       // console.log(banks)
+        // console.log(banks)
         for (let key in this.banks) {
-            let value = this.banks[key]
+            let value = this.banks[key];
             if (value === "READY") {
                 edit_list.push(
-                    <div className="gf-form-inline">
-                        <h4> Bank {key}</h4>
-                        <input  type="text" value={key}/>
-                        <br/>
-                        <h5> Channel 1 </h5>
-                        <input style={{color: "black", background: "white"}} type="text" value={"Bank " + key + "Channel 1"}/>
-                        <h5> Channel 2 </h5>
-                        <input style={{color: "black", background: "white"}} type="text" value={"Bank " + key + "Channel 2"}/>
-                        <h5> Channel 3 </h5>
-                        <input style={{color: "black", background: "white"}} type="text" value={"Bank " + key + "Channel 3"}/>
-                        <h5> Channel 4 </h5>
-                        <input style={{color: "black", background: "white"}} type="text" value={"Bank " + key + "Channel 4"}/>
-                        <h5> Channel 5 </h5>
-                        <input style={{color: "black", background: "white"}} type="text" value={"Bank " + key + "Channel 5"}/>
-                        <h5> Channel 6 </h5>
-                        <input style={{color: "black", background: "white"}} type="text" value={"Bank " + key + "Channel 6"}/>
-                        <h5> Channel 7 </h5>
-                        <input style={{color: "black", background: "white"}} type="text" value={"Bank " + key + "Channel 7"}/>
-                        <h5> Channel 8 </h5>
-                        <input style={{color: "black", background: "white"}} type="text" value={"Bank " + key + "Channel 8"}/>
+                    <div className="gf-form-group">
+                        <div className="row">
+                            <label style={{marginLeft: 20, marginRight: 10, fontSize: 15}}> Bank {key}</label>
+                            <input className="col-sm-6" style={{
+                                color: "black",
+                                background: "white",
+                                border: "1px solid black",
+                                borderRadius: 2,
+                            }} type="text" value={"Bank " + key}/>
+                        </div>
+                        <div className="row">
+                            <label style={{marginLeft: 30, marginRight: 10, fontSize: 15}}> Channel 1 </label>
+                            <input className="col-sm-6" style={{
+                                color: "black",
+                                background: "white",
+                                border: "1px solid black",
+                                borderRadius: 2,
+                            }} type="text" value={"Bank " + key + ", Channel 1"}/>
+                        </div>
+                        <div className="row">
+                            <label style={{marginLeft: 30, marginRight: 10, fontSize: 15}}> Channel 2 </label>
+                            <input className="col-sm-6" style={{
+                                color: "black",
+                                background: "white",
+                                border: "1px solid black",
+                                borderRadius: 2,
+                            }} type="text" value={"Bank " + key + ", Channel 2"}/>
+                        </div>
+                        <div className="row">
+                            <label style={{marginLeft: 30, marginRight: 10, fontSize: 15}}> Channel 3 </label>
+                            <input className="col-sm-6" style={{
+                                color: "black",
+                                background: "white",
+                                border: "1px solid black",
+                                borderRadius: 2,
+                            }} type="text" value={"Bank " + key + ", Channel 3"}/>
+                        </div>
+                        <div className="row">
+                            <label style={{marginLeft: 30, marginRight: 10, fontSize: 15}}> Channel 4 </label>
+                            <input className="col-sm-6" style={{
+                                color: "black",
+                                background: "white",
+                                border: "1px solid black",
+                                borderRadius: 2,
+                            }} type="text" value={"Bank " + key + ", Channel 4"}/>
+                        </div>
+                        <div className="row">
+                            <label style={{marginLeft: 30, marginRight: 10, fontSize: 15}}> Channel 5 </label>
+                            <input className="col-sm-6" style={{
+                                color: "black",
+                                background: "white",
+                                border: "1px solid black",
+                                borderRadius: 2,
+                            }} type="text" value={"Bank " + key + ", Channel 5"}/>
+                        </div>
+                        <div className="row">
+                            <label style={{marginLeft: 30, marginRight: 10, fontSize: 15}}> Channel 6 </label>
+                            <input className="col-sm-6" style={{
+                                color: "black",
+                                background: "white",
+                                border: "1px solid black",
+                                borderRadius: 2,
+                            }} type="text" value={"Bank " + key + ", Channel 6"}/>
+                        </div>
+                        <div className="row">
+                            <label style={{marginLeft: 30, marginRight: 10, fontSize: 15}}> Channel 7 </label>
+                            <input className="col-sm-6" style={{
+                                color: "black",
+                                background: "white",
+                                border: "1px solid black",
+                                borderRadius: 2,
+                            }} type="text" value={"Bank " + key + ", Channel 7"}/>
+                        </div>
+                        <div className="row">
+                            <label style={{marginLeft: 30, marginRight: 10, fontSize: 15}}> Channel 8 </label>
+                            <input className="col-sm-6" style={{
+                                color: "black",
+                                background: "white",
+                                border: "1px solid black",
+                                borderRadius: 2,
+                            }} type="text" value={"Bank " + key + ", Channel 8"}/>
+                        </div>
                     </div>
                 )
             }
         }
-        //console.log(typeof(banks))
-       // const banks = JSON.stringify(test['bank'])
-      //  console.log(test)
-      //   for (let key in test) {
-      //       if (key === 'bank') {
-      //           value = test[key];
-      //       }
-      //       // Use `key` and `value`
-      //   }
+        return (
+            <div>
+                {edit_list}
+            </div>
+        );
+    };
 
+    renderItem = () => <div>{this.makeEditRow()}</div>;
+
+    render() {
 
         return (
-            <div className="gf-form-inline" style={{backgroundColor: "#888"}}>
-                Hello
-                {edit_list}
-                <div className="row text-center" style={{marginTop: "10px", marginBottom: "10px", marginLeft: -25}}>
+                <div className="panel-plan" style={{ height: 550, backgroundColor: "#888", padding: 10, border: "3px solid #111", borderRadius: 7 }}>
+                    <h2 style={{color: "white"}}>Edit Bank & Channel Names</h2>
+                    <div style={{overflowX: "hidden", overflowY: "auto", maxHeight:440, borderRadius: 3}}>
+                        <form>
+                            <ReactList
+                                itemRenderer={this.renderItem}
+                                length={1}
+                                type={"uniform"}
+                            />
+                        </form>
+                    </div>
+                <div className="row text-center" style={{marginTop: "10px", marginBottom: "10px"}}>
                     <div className="col-sm-4">
                         <button type="button"
                                 className={"btn btn-block btn-success"}  style={{ marginBottom: 10,  width: 95, height: 40, borderRadius: 5, textAlign: "center", fontSize: 20}}>
