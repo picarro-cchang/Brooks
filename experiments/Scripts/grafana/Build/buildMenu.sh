f855357cc0a55a61b2d84a6bbe1fc260fddc5153
gsornsen <gsornsen@gmail.com>
2019-08-21 11:44:05 -0700

    Usability improvments

--- experiments/Scripts/grafana/Build/buildMenu.sh
+++ experiments/Scripts/grafana/Build/buildMenu.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/bash -i
 
 # This script provides a menu for Grafana builds
-
+source ~/.bashrc
 gitDir="${HOME}/git"
 grafanaDir="${gitDir}/host/experiments/grafana/src/github.com/grafana/grafana"
+scriptsDir="${gitDir}/host/experiments/Scripts/grafana/Build"
+
 
 while : # Loop forever
 do
@@ -27,11 +29,11 @@ echo -n "Select an option: "
 read choice
 
 case $choice in
-	1) ./buildFrontEnd.sh;;
-	2) ./buildBackEnd.sh;;
-	3) ./buildDebianPackage.sh;;
-	4) ./startGrafanaServer.sh; sleep 1s; clear;;
-	5) chromium-browser --start-fullscreen http://localhost:3000;;
+	1) $scriptsDir/buildFrontEnd.sh;;
+	2) $scriptsDir/buildBackEnd.sh;;
+	3) $scriptsDir/buildDebianPackage.sh;;
+	4) gnome-terminal -- $scriptsDir/startGrafanaServer.sh;;
+	5) gnome-terminal -- chromium-browser --start-fullscreen http://localhost:3000;;
 	6) exit;;
 	*) echo "\"$choice\" is not an option."; sleep 1s ;;
 esac
