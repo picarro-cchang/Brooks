d3d8d99d7952ec977734eacafa9424d4dea93932
Petr <pkhromov@picarroc.com>
2019-08-23 13:33:20 -0700

    LOLogger Client fix for when server is not available

--- experiments/LOLogger/LOLoggerClient.py
+++ experiments/LOLogger/LOLoggerClient.py
@@ -11,20 +11,18 @@ class LOLoggerClient():
         A wrapper around CmdFIFO proxy for logger
     """
 
-    def __init__(self,
-                 client_name="UnnamedClient",
-                 logger_address="localhost",
-                 port=rpc_ports["logger"],
-                 ip="",
-                 verbose=False):
+    def __init__(self, client_name="UnnamedClient", logger_address="localhost", port=rpc_ports["logger"], ip="", verbose=False):
         self.logger_address = logger_address
         self.port = port
         self.client_name = client_name
         self.ip = ip
         self.verbose = verbose
-        self.lologger = CmdFIFO.CmdFIFOServerProxy(
-            f"http://{self.logger_address}:{self.port}",
-            ClientName=self.client_name)
+        server_path = f"http://{self.logger_address}:{self.port}"
+        try:
+            self.lologger = CmdFIFO.CmdFIFOServerProxy(server_path, ClientName=self.client_name)
+        except:
+            print(f"WARNING!!! Failed to connect to LOLogger RPC on {server_path}! Forcing local verbose")
+            self.verbose = True
 
     def Log(self, message, level=0):
         ClientTimestamp = str(timeutils.get_local_timestamp())
