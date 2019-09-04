d9ee8639842b9e7742e4c073d720fb09b9beae87
epeatfield@picarro.com <epeatfield@picarro.com>
2019-09-04 14:01:03 -0700

    Changes for potential home page

--- experiments/grafana/plugins/button-panel/src/components/ButtonLayout.tsx
+++ experiments/grafana/plugins/button-panel/src/components/ButtonLayout.tsx
@@ -5,28 +5,32 @@ import './button.css';
 export default class ButtonLayout extends Component<any, any> {
     render() {
         return (
-          <div className="btn-container">
-              <div className="row">
-                  <div className="btn-label-container">
-                      <h4>Sampler Setup</h4>
-                      <a className="btn-panel-link btn-sampler" href="/d/xemz2IvWk"></a>
+            <div className="row-test">
+              <div className="btn-container">
+                  <div className="row tts">
+                      <div className="btn-label-container">
+                          <h4>Sampler Setup</h4>
+                          <a className="btn-panel-link btn-sampler" href="/d/xemz2IvWk"></a>
+                      </div>
+                      <div className="btn-label-container">
+                          <h4>Analyzer Graphs</h4>
+                          <a className="btn-panel-link btn-analyzer" href="/d/0C5NddvZk"></a>
+                      </div>
                   </div>
-                  <div className="btn-label-container">
-                      <h4>Analyzer Graphs</h4>
-                      <a className="btn-panel-link btn-analyzer" href="/d/0C5NddvZk"></a>
+                  <div className="row tts">
+                      <div className="btn-label-container ">
+                          <h4>Valve Graphs</h4>
+                          <a className="btn-panel-link btn-valve-graphs" href="/d/pV3XhcDWz"></a>
+                      </div>
+                      <div className="btn-label-container ">
+                          <h4>Data Tables</h4>
+                          <a className="btn-panel-link btn-tables" href="/d/vCQ3IOvWk"></a>
+                      </div>
                   </div>
               </div>
-              <div className="row">
-                  <div className="btn-label-container ">
-                      <h4>Valve Graphs</h4>
-                      <a className="btn-panel-link btn-valve-graphs" href="/d/pV3XhcDWz"></a>
-                  </div>
-                  <div className="btn-label-container ">
-                      <h4>Data Tables</h4>
-                      <a className="btn-panel-link btn-tables" href="/d/vCQ3IOvWk"></a>
-                  </div>
-              </div>
-          </div>
+                <div className="vertical-rule">
+                </div>
+            </div>
         );
     }
 }
