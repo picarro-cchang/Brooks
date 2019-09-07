f753a5ac782158fc68b0b328bd99ad299a08e284
gsornsen <gsornsen@gmail.com>
2019-09-06 21:09:48 -0700

    Dirty bash script to start simulator on virtual network interface

---
+++ experiments/Simulators/start_simulators.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set up virtual network interfaces
+sudo ip link delete dummy0
+sudo ip link delete dummy1
+sudo ip link delete dummy2
+sudo ip link add dummy0 type dummy
+sudo ip link add dummy1 type dummy
+sudo ip link add dummy2 type dummy
+sudo ip addr add 192.168.10.101/24 brd + dev dummy0
+sudo ip addr add 192.168.10.102/24 brd + dev dummy1
+sudo ip addr add 192.168.10.103/24 brd + dev dummy2
+
+
+# Start simulators
+python mysim.py dummy0 '{"chassis": "4123", "analyzer": "AMADS", "analyzer_num": "3001", "species": ["NH3", "HF", "H2O"], "source": "analyze_AMADS_LCT", "mode": "AMADS_LCT_mode", "interval": 1.1}'
+# python mysim.py dummy1 '{"chassis": "4357", "analyzer": "SBDS", "analyzer_num": "3002", "species": ["HCl", "H2O"], "source": "analyze_SADS", "mode": "HCl_mode", "interval": 1.2}' &
+# python mysim.py dummy2 '{"chassis": "4532", "analyzer": "BFADS", "analyzer_num": "3003", "species": ["H2S", "HDO"], "source": "analyze_BFADS", "mode": "BFADS_mode", "interval": 1.3}' &
+
