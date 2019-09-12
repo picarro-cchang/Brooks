09d047cb234bbdd6586caa2257ec9273947177a4
epeatfield@picarro.com <epeatfield@picarro.com>
2019-09-12 14:36:53 -0700

    Fixed home page links to maintain time range

--- experiments/grafana/plugins/button-panel/src/components/ButtonLayout.tsx
+++ experiments/grafana/plugins/button-panel/src/components/ButtonLayout.tsx
@@ -4,24 +4,28 @@ import './button.css';
 
 export default class ButtonLayout extends Component<any, any> {
     render() {
+        console.log(this.props.timeRange.raw.to);
+        console.log(this.props.timeRange.raw.from);
+        const from = this.props.timeRange.raw.from;
+        const to = this.props.timeRange.raw.to;
         return (
             <div className="row-test">
               <div className="btn-container">
                       <div className="btn-label-container" style={{gridColumnStart: 1, gridColumnEnd: 2}}>
                           <h4>Flow Setup</h4>
-                          <a className="btn-panel-link btn-sampler" href="/d/xemz2IvWk"></a>
+                          <a className="btn-panel-link btn-sampler" href={"/d/xemz2IvWk/sampler-setup?from=" + from + "&to=" + to}></a>
                       </div>
                       <div className="btn-label-container">
                           <h4>Analyzer Graphs</h4>
-                          <a className="btn-panel-link btn-analyzer" href="/d/rHPC-eKZz"></a>
+                          <a className="btn-panel-link btn-analyzer" href={"/d/rHPC-eKZz/analyzer-output-graphs?from=" + from + "&to=" + to}></a>
                       </div>
                       <div className="btn-label-container " style={{gridColumnStart: 1, gridColumnEnd: 2}}>
                           <h4>Graphs by Port</h4>
-                          <a className="btn-panel-link btn-analyzer" href="/d/GTo10zcZz"></a>
+                          <a className="btn-panel-link btn-analyzer" href={"/d/GTo10zcZz/concentration-by-valve?from=" + from + "&to=" + to}></a>
                       </div>
                       <div className="btn-label-container ">
                           <h4>Data Tables</h4>
-                          <a className="btn-panel-link btn-tables" href="/d/1iflIi5Wz"></a>
+                          <a className="btn-panel-link btn-tables" href={"/d/1iflIi5Wz/summary?from=" + from + "&to=" + to}></a>
                       </div>
               </div>
             </div>
