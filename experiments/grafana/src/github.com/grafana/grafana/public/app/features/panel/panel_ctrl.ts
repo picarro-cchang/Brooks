c0a8e68eaadb92418b36970d736d9015af29ece9
epeatfield@picarro.com <epeatfield@picarro.com>
2019-09-19 16:18:08 -0700

    Removed dropdown menu from viewer on panel header

--- experiments/grafana/src/github.com/grafana/grafana/public/app/features/panel/panel_ctrl.ts
+++ experiments/grafana/src/github.com/grafana/grafana/public/app/features/panel/panel_ctrl.ts
@@ -121,12 +121,15 @@ export class PanelCtrl {
 
   getMenu() {
     const menu = [];
-    menu.push({
-      text: 'View',
-      click: 'ctrl.viewPanel();',
-      icon: 'gicon gicon-viewer',
-      shortcut: 'v',
-    });
+    if (this.dashboard.meta.canEdit) {
+      menu.push({
+        text: 'View',
+        click: 'ctrl.viewPanel();',
+        icon: 'gicon gicon-viewer',
+        shortcut: 'v',
+      });
+    }
+
 
     if (this.dashboard.meta.canEdit) {
       menu.push({
@@ -138,23 +141,28 @@ export class PanelCtrl {
       });
     }
 
-    menu.push({
-      text: 'Share',
-      click: 'ctrl.sharePanel();',
-      icon: 'fa fa-fw fa-share',
-      shortcut: 'p s',
-    });
+    if (this.dashboard.meta.canEdit) {
+      menu.push({
+        text: 'Share',
+        click: 'ctrl.sharePanel();',
+        icon: 'fa fa-fw fa-share',
+        shortcut: 'p s',
+      });
+    }
 
     // Additional items from sub-class
     menu.push(...this.getAdditionalMenuItems());
 
     const extendedMenu = this.getExtendedMenu();
-    menu.push({
-      text: 'More ...',
-      click: '',
-      icon: 'fa fa-fw fa-cube',
-      submenu: extendedMenu,
-    });
+
+    if (this.dashboard.meta.canEdit) {
+      menu.push({
+        text: 'More ...',
+        click: '',
+        icon: 'fa fa-fw fa-cube',
+        submenu: extendedMenu,
+      });
+    }
 
     if (this.dashboard.meta.canEdit) {
       menu.push({ divider: true, role: 'Editor' });
@@ -187,10 +195,12 @@ export class PanelCtrl {
       });
     }
 
-    menu.push({
-      text: 'Panel JSON',
-      click: 'ctrl.editPanelJson(); dismiss();',
-    });
+    if (this.dashboard.meta.canEdit) {
+      menu.push({
+        text: 'Panel JSON',
+        click: 'ctrl.editPanelJson(); dismiss();',
+      });
+    }
 
     this.events.emit('init-panel-actions', menu);
     return menu;
