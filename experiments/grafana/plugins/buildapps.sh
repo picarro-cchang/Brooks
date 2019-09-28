ebafe88c838acf005acb91cf70191a23e4effd84
gsornsen <gsornsen@gmail.com>
2019-09-27 20:03:37 -0700

    Edited buildapps.sh to build all plugins defined in , provisioning only production apps in loadapps script

--- experiments/grafana/plugins/buildapps.sh
+++ experiments/grafana/plugins/buildapps.sh
@@ -1,21 +1,32 @@
-cd ${HOME}/git/host/experiments/grafana/plugins/button-panel
-yarn install
-yarn build
-cd ../analyzer-graphs-app
-yarn install
-yarn build
-cd ../concentration-by-valve-app
-yarn install
-yarn build
-cd ../state-machine-app
-yarn install 
-yarn build
-cd ../simpod-json-datasource
-yarn install
-yarn build
-cd ../summary-app
-yarn install
-yarn build
-cd ../current-values-app
-yarn install
-yarn build
+#!/bin/bash -e
+
+# Location for grafana plugins when running from source
+grafanaSourcePluginDestDir=${HOME}/git/host/experiments/grafana/src/github.com/grafana/grafana/data/plugins/
+# Location of custom grafana plugins in repo
+pluginSourceDir=${HOME}/git/host/experiments/grafana/plugins
+# List of plugins to be built
+pluginList=('
+    button-panel
+    analyzer-graphs-app
+    concentration-by-valve-app
+    simpod-json-datasource
+    current-values-app
+    state-machine-app
+    ')
+
+# Make the grafana source plugin directory if it doesn't exist
+mkdir -p ${grafanaSourcePluginDestDir}
+# Remove any folders/symlinks if they exist
+cd ${grafanaSourcePluginDestDir}
+rm -rf *
+
+# Create individual symlinks and build
+for plugin in ${pluginList};do
+    cd ${pluginSourceDir}/${plugin}
+    printf "\n"; printf "Creating ${plugin} symlink"; printf "\n\n"
+    ln -vs ${PWD} ${grafanaSourcePluginDestDir}/${plugin}
+    printf "\n"; printf "Building ${plugin}"; printf "\n\n"
+    yarn install
+    yarn build
+done
+
