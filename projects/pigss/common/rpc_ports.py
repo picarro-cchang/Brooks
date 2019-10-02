204a7eb30599dd96393ab086ed53dd244120e3c9
Arekseon <YOUR EMAIL ADDRESS>
2019-10-01 20:54:29 -0700

    lologger added

---
+++ projects/pigss/common/rpc_ports.py
@@ -0,0 +1,9 @@
+rpc_ports = {
+    "aiohttp": 8080,
+    "instrument_drivers": 33000,
+    "mfc_drivers": 33020,
+    "relay_drivers": 33030,
+    "piglet_drivers": 33040,
+    "logger": 33050,
+    "madmapper": 33051
+}
