5dc0a02333057e23dcffa01ed1fcf62ac9169755
gsornsen <gsornsen@gmail.com>
2019-10-02 10:48:14 -0700

    Added rpc port for analyzer simulators

--- projects/pigss/common/rpc_ports.py
+++ projects/pigss/common/rpc_ports.py
@@ -5,5 +5,8 @@ rpc_ports = {
     "relay_drivers": 33030,
     "piglet_drivers": 33040,
     "logger": 33050,
-    "madmapper": 33051
+    "madmapper": 33051,
+    "db_decimator": 33052,
+    # Leave enough ports for as many simulators as needed
+    "analyzer_simulator_base": 33060
 }
