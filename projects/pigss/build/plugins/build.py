24740210a60fa03dae6fb6c51d3728c27fe1d766
gsornsen <gsornsen@picarro.com>
2019-10-28 17:06:54 -0700

    Added python script to build PiGSS grafana plugins

---
+++ projects/pigss/build/plugins/build.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from core.utilities.grootilities.src.grootility import GROOTility
+# TODO: Replace import -- pip install package and remove submodule
+
+
+def build():
+    with GROOTility('build') as GROOT:
+        GROOT.build_plugins()
+        GROOT._deploy_plugins()
+
+
+if __name__ == '__main__':
+    build()
