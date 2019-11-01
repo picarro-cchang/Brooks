67aece5a0bdd1a0e0d910aca695c9ce86ce060be
picarro-dileep <dpandey@picarro.com>
2019-11-01 00:37:57 -0700

    added newlines

--- projects/pigss/front_end/grafana/picarro/plugins/statusApp/src/panels/SupervisorStatus/module.tsx
+++ projects/pigss/front_end/grafana/picarro/plugins/statusApp/src/panels/SupervisorStatus/module.tsx
@@ -3,5 +3,3 @@ import { SupervisorStatusPanel } from './src/components/SupervisorStatusPanel';
 import { SupervisorStatusProps } from './src/types';
 
 export const plugin = new PanelPlugin<SupervisorStatusProps>(SupervisorStatusPanel);
-
-// plugin.setDefaults();
