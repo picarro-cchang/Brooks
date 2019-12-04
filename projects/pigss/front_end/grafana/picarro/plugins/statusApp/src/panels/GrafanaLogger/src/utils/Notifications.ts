dc0b20040690a0ada48e44dcf6fc64b264ebe002
Dileep Pandey <53542555+picarro-dileep@users.noreply.github.com>
2019-12-03 23:46:51 -0800

    Update projects/pigss/front_end/grafana/picarro/plugins/statusApp/src/panels/GrafanaLogger/src/utils/Notifications.ts
    Co-Authored-By: Gerald Sornsen <gsornsen@gmail.com>

--- projects/pigss/front_end/grafana/picarro/plugins/statusApp/src/panels/GrafanaLogger/src/utils/Notifications.ts
+++ projects/pigss/front_end/grafana/picarro/plugins/statusApp/src/panels/GrafanaLogger/src/utils/Notifications.ts
@@ -1,7 +1,6 @@
 import { toast } from 'react-toastify';
 
 export const notifyError = (message: string) => {
-  console.error(message);
   toast.error(message, {
     position: 'bottom-right',
     autoClose: 8000,
