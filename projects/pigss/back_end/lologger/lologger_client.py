b5d24b4f710b0b114f14b9630630957278ad51f1
picarro-pkhromov <39276567+picarro-pkhromov@users.noreply.github.com>
2019-12-10 00:11:29 -0800

    Update lologger_client.py
    add resubmitting local logs on reconnecting to lologger server if some logs happened while lologger server was down

--- projects/pigss/back_end/lologger/lologger_client.py
+++ projects/pigss/back_end/lologger/lologger_client.py
@@ -1,14 +1,21 @@
+from collections import deque as deque
 import common.CmdFIFO as CmdFIFO
 import common.timeutils as timeutils
 from common.rpc_ports import rpc_ports
 
-ATTEMPT_TO_RECONNECT_AFTER_N_LOGS = 5
+ATTEMPT_TO_RECONNECT_AFTER_N_LOGS = 1
 
 class LOLoggerClient():
     """
         LOLogger client.
     """
-    def __init__(self, client_name="UnnamedClient", logger_address="localhost", port=rpc_ports["logger"], ip="", verbose=False):
+    def __init__(self,
+                 client_name="UnnamedClient",
+                 logger_address="localhost",
+                 port=rpc_ports["logger"],
+                 ip="",
+                 verbose=False,
+                 local_logs_storage=200):
         self.logger_address = logger_address
         self.port = port
         self.client_name = client_name
@@ -17,9 +24,10 @@ class LOLoggerClient():
         self.connected = False
         self.server_path = f"http://{self.logger_address}:{self.port}"
         self.reconnect_counter = 0
-        self.local_logs = []
+        self.local_logs_storage = local_logs_storage
+        self.local_logs = deque([], self.local_logs_storage)
 
-        if self.connect_to_lollogger(True):
+        if self.__connect_to_lollogger(True):
             self.connected = True
         else:
             print(f"WARNING!!! Failed to connect to LOLogger RPC on {self.server_path}! Forcing local verbose")
@@ -27,16 +35,17 @@ class LOLoggerClient():
 
     def Log(self, message, level=0):
         ClientTimestamp = str(timeutils.get_local_timestamp())
+        log_submitted = False
         if self.verbose:
             print(message)
         if not self.connected:
             if self.reconnect_counter >= ATTEMPT_TO_RECONNECT_AFTER_N_LOGS:
                 self.reconnect_counter = 0
-                if self.connect_to_lollogger(True):
+                if self.__connect_to_lollogger(False):
                     self.connected = True
+                    self.__resubmit_local_logs()
             else:
                 self.reconnect_counter += 1
-
 
         if self.connected:
             try:
@@ -45,20 +54,27 @@ class LOLoggerClient():
                                        client_name=self.client_name,
                                        ip=self.ip,
                                        client_timestamp=ClientTimestamp)
+                log_submitted = True
             except Exception as e:
                 print(f"{e}")
                 print(
                     f"{ClientTimestamp}:WARNING!!! Failed to connect to LOLogger RPC on {self.server_path}! Forcing local verbose")
                 self.verbose = True
                 self.connected = False
-        else:
+        if not log_submitted:
             self.local_logs.append({"log_message":message,
                                        "level":level,
                                        "client_name":self.client_name,
                                        "ip":self.ip,
                                        "client_timestamp":ClientTimestamp})
 
-    def connect_to_lollogger(self, verbose=False):
+    def __resubmit_local_logs(self):
+        if self.connected:
+            for log_message in self.local_logs:
+                self.lologger.LogEvent(**log_message)
+            self.local_logs = deque([], self.local_logs_storage)
+
+    def __connect_to_lollogger(self, verbose=False):
         try:
             self.lologger = CmdFIFO.CmdFIFOServerProxy(self.server_path, ClientName=self.client_name)
             self.lologger.CmdFIFO.PingFIFO()
