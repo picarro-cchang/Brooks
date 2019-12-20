ccd5eda297b1600b5575fd57f4e0f1483e027de9
epeatfield@picarro.com <epeatfield@picarro.com>
2019-12-20 12:09:24 -0800

    Completed PlanLoadPanel

--- projects/pigss/front_end/grafana/picarro/plugins/stateMachineApp/src/panels/StateMachine/components/__tests__/PlanLoadPanel.spec.tsx
+++ projects/pigss/front_end/grafana/picarro/plugins/stateMachineApp/src/panels/StateMachine/components/__tests__/PlanLoadPanel.spec.tsx
@@ -6,7 +6,9 @@ import 'jest-styled-components';
 import PlanLoadPanel from '../PlanLoadPanel';
 import { PlanLoadPanelOptions } from '../../types';
 
-const mockClick = jest.fn();
+const mockClick = jest.fn((element) => {return element});
+const apiLoc = `${window.location.hostname}:8000/controller`;
+const socketURL = `ws://${apiLoc}/ws`;
 const mockUpdateFileName = jest.fn();
 const defaultProps: PlanLoadPanelOptions = {
     plan: {
@@ -102,6 +104,8 @@ const defaultProps: PlanLoadPanelOptions = {
 describe('<PlanLoadPanel />', () => {
     const wrapper = shallow(<PlanLoadPanel {...defaultProps} />);
     const instance = wrapper.instance() as PlanLoadPanel;
+    const server = new WS(socketURL);
+    const client = new WebSocket(socketURL);
 
     it('Match Snapshot', () => {
         expect(wrapper).toMatchSnapshot();
@@ -112,9 +116,9 @@ describe('<PlanLoadPanel />', () => {
         instance.renderItem(0, 0);
         expect(instance.renderItem).toHaveBeenCalled();
 
-        const test = mount(<PlanLoadPanel {...defaultProps}/>).find('ReactList');
-        expect(test).toMatchSnapshot();
-        expect(test.find('button').at(0).text()).toEqual('test.pln');
+        const fileList = mount(<PlanLoadPanel {...defaultProps}/>).find('ReactList');
+        expect(fileList).toMatchSnapshot();
+        expect(fileList.find('button').at(0).text()).toEqual('test.pln');
     });
 
     it('Cancel', () => {
@@ -123,15 +127,31 @@ describe('<PlanLoadPanel />', () => {
         expect(mockClick).toBeCalled();
     });
 
-    it('Load File', () => {
-        // const test = mount(<PlanLoadPanel {...defaultProps}/>).find('ReactList');
-        // expect(test).toMatchSnapshot();
-        // expect(test.find('button').at(0).text()).toEqual('test.pln');
+    it('Load File', async () => {
+        mockClick.mockClear();
+        await server.connected;
+        const fileButton = mount(<PlanLoadPanel {...defaultProps}/>).find('ReactList').find('button').at(0);
+        fileButton.simulate('click');
+        const element = mockClick.mock.calls[0][0];
+        client.send(element);
+        expect(mockUpdateFileName).toHaveBeenCalled();
+        expect(mockClick).toHaveBeenCalled();
+        await expect(server).toReceiveMessage({element: "plan_load_filename", name: "test.pln"})
+        expect(server).toHaveReceivedMessages([{element: "plan_load_filename", name: "test.pln"}])
+        mockClick.mockClear();
+        server.close;
     });
 
-    it('Delete File', () => {
-        // const test = mount(<PlanLoadPanel {...defaultProps}/>).find('ReactList');
-        // expect(test).toMatchSnapshot();
-        // expect(test.find('button').at(0).text()).toEqual('test.pln');
+    it('Delete File', async () => {
+        await server.connected;
+        const deleteFile = mount(<PlanLoadPanel {...defaultProps}/>).find('ReactList').find('button').at(1);
+        deleteFile.simulate('click');
+        const element = mockClick.mock.calls[0][0];
+        client.send(element);
+        expect(mockClick).toHaveBeenCalled();
+        await expect(server).toReceiveMessage({element: "plan_delete_filename", name: "test.pln"})
+        expect(server).toHaveReceivedMessages([{element: "plan_delete_filename", name: "test.pln"}])
+        mockClick.mockClear();
+        server.close;
     });
 });
