5b12ad227b63f1f9b04b7fd727b1db9647e6402d
epeatfield@picarro.com <epeatfield@picarro.com>
2020-01-30 16:32:50 -0800

    PIG-495, PIG-496 Front-end and Back-end changes for scalable dashboards

--- projects/pigss/back_end/servers/species_type_service.py
+++ projects/pigss/back_end/servers/species_type_service.py
@@ -5,18 +5,21 @@ Provides a JSON datasource for Grafana which serves up information
 """
 import json
 import time
+from traceback import format_exc
 
 from aiohttp import web
 from aioinflux import iterpoints
 
+from common.influx_connection import InfluxDBInstance
+from influxdb.exceptions import InfluxDBClientError
 from back_end.database_access.aio_influx_database import AioInfluxDBWriter
 from back_end.lologger.lologger_client import LOLoggerClient
 from back_end.servers.service_template import ServiceTemplate
 
-log = LOLoggerClient(client_name="PortHistoryService", verbose=True)
+log = LOLoggerClient(client_name="SpeciesTypeService", verbose=True)
 
 
-class PortHistoryService(ServiceTemplate):
+class SpeciesTypeService(ServiceTemplate):
     def __init__(self):
         super().__init__()
 
@@ -26,6 +29,13 @@ class PortHistoryService(ServiceTemplate):
 
     async def on_startup(self, app):
         log.debug("Species Type service is starting up")
+        self.app["config"]= self.app["farm"].config.get_gdg_plugin_config()
+        self.app["config"]["species"] = self.app["farm"].config.get_species()
+
+
+        # Create influxdb connection
+        self.app["db_client"] = InfluxDBInstance(self.app["config"]["database"]).get_instance()
+
 
     async def on_shutdown(self, app):
         log.debug("Species Type service is shutting down")
@@ -47,11 +57,11 @@ class PortHistoryService(ServiceTemplate):
 
     async def search(self, request):
         """
-        description: Gets the names of the available banks or the available ports
+        description: Gets the names of the available species or keys
 
         tags:
             -   Species Type service
-        summary:  Gets the names of the available banks or the available ports
+        summary:  Gets the names of the available species or keys
 
         consumes:
             -   application/json
@@ -79,6 +89,16 @@ class PortHistoryService(ServiceTemplate):
         #  with keys "range" and "type".
 
         # start_ms and end_ms are the Grafana time range for the plots
-        species = [{ }]
+        measurements = self.app["config"]["database"]["measurements"]
+        try:
+            field_keys_resultset = self.app["db_client"].query(f"SHOW FIELD KEYS FROM {measurements}")
+        except InfluxDBClientError:
+            log.debug(f"SpeciesTypeService: {format_exc()}")
+        result = None
+        for datum in field_keys_resultset:
+            result = datum
+
+        field_keys = [field['fieldKey'] for field in result]
+        user_species = self.app["config"]["species"]
         
-        return web.json_response(species)
+        return web.json_response(list(filter(lambda x: x in field_keys, user_species)))
---
+++ projects/pigss/front_end/grafana/picarro/plugins/dynamicConcentrationByValveApp/README.md
@@ -0,0 +1,3 @@
+#Concentration Graphs
+
+Grafana Panel graphs for each Valve/Port available for a specified species. 
