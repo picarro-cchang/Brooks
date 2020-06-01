047d69be70daa44fbd834d8d2e4963e023d6d374
picarro-epeatfield <52256471+picarro-epeatfield@users.noreply.github.com>
2020-06-01 10:28:21 -0700

    Update projects/pigss/front_end/grafana/picarro/plugins/userDataApp/src/module.tsx
    Co-authored-by: Gerald Sornsen <gsornsen@gmail.com>

--- projects/pigss/front_end/grafana/picarro/plugins/userDataApp/src/module.tsx
+++ projects/pigss/front_end/grafana/picarro/plugins/userDataApp/src/module.tsx
@@ -2,7 +2,6 @@ import { UserDataAppConfigCtrl } from './legacy/config';
 // @ts-ignore
 import { AppPlugin } from '@grafana/ui';
 // @ts-ignore
-// import { AppPlugin } from '@grafana/data';
 
 // Legacy exports just for update/enable and disable
 export { UserDataAppConfigCtrl as ConfigCtrl };
